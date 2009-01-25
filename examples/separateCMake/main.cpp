d89ea5add61b6fee52a7fae00a061508b7cf1f7c
Anton Deguet <anton.deguet@jhu.edu>
2009-01-25 19:00:01 +0000

    public trunk: added example separateCMake used in build instructions
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/public/trunk@34 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ examples/separateCMake/main.cpp
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  $Id: main.cpp,v 1.3 2007/05/31 21:02:50 anton Exp $
+
+  Author(s): Anton Deguet
+  Created on: 2006-11-20
+
+  (C) Copyright 2006-2007 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+
+#include "newLibrary.h"
+
+int main(void)
+{
+    newClass newObject;
+    newObject.PerformRandomSVD();
+}
+
