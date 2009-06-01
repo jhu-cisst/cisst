e5cebc96860d6935c6664a5f3feec759699b2579
Balazs Vagvolgyi <balazs.vagvolgyi@jhu.edu>
2009-06-01 17:45:50 +0000

    branches main: cisstOSAbstraction - Log message added to mutex initialization in osaThreadSignal constructor
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/main@391 0959eb1a-6956-4f60-947b-6092a4e22c97

--- libs/code/cisstOSAbstraction/osaThreadSignal.cpp
+++ libs/code/cisstOSAbstraction/osaThreadSignal.cpp
@@ -236,7 +236,9 @@ osaThreadSignal::osaThreadSignal()
 #endif
 
 #if (CISST_OS == CISST_LINUX_RTAI) || (CISST_OS == CISST_LINUX) || (CISST_OS == CISST_DARWIN) || (CISST_OS == CISST_SOLARIS)
-    pthread_mutex_init(&INTERNALS(gnuMutex), 0);
+    if (pthread_mutex_init(&INTERNALS(gnuMutex), 0) != 0) {
+        CMN_LOG(1) << "Class osaMutex: error in constructor \"" << strerror(errno) << "\"" << std::endl;
+    }
     pthread_cond_init(&INTERNALS(gnuCondition), 0);
     INTERNALS(Condition_State) = 0;
 #endif
