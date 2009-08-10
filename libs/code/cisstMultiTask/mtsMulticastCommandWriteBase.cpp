56fc9870631cc7deaebd56fb2705f2fc0f87741c
Min Yang Jung <myj@jhu.edu>
2009-08-10 16:34:01 +0000

    branches/main: bug fixed.
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/main@651 0959eb1a-6956-4f60-947b-6092a4e22c97

--- libs/code/cisstMultiTask/mtsMulticastCommandWriteBase.cpp
+++ libs/code/cisstMultiTask/mtsMulticastCommandWriteBase.cpp
@@ -24,14 +24,14 @@ http://www.cisst.org/cisst/license.txt.
 
 void mtsMulticastCommandWriteBase::AddCommand(BaseType * command) {
     if (command) {
-#if CISST_MTS_HAS_ICE
+#ifndef CISST_MTS_HAS_ICE
         if (typeid(*(command->GetArgumentPrototype())) != typeid(*(this->GetArgumentPrototype()))) {
             CMN_LOG_INIT_ERROR << "Class mtsMulticastCommandWriteBase: AddCommand: command argument type don't match" << std::endl;
             exit(0);
         } else {
 #endif
             this->Commands.push_back(command);
-#if CISST_MTS_HAS_ICE
+#ifndef CISST_MTS_HAS_ICE
         }
 #endif
     }
