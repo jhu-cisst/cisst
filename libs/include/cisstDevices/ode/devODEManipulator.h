d91ab1e91f79b9e760492bf0cbbd188c41ef2c50
Simon Leonard <sleonard@jhu.edu>
2010-03-26 18:17:55 +0000

    added ode .h
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/main@1342 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ libs/include/cisstDevices/ode/devODEManipulator.h
@@ -0,0 +1,108 @@
+/*
+
+  Author(s): Simon Leonard
+  Created on: Nov 11 2009
+
+  (C) Copyright 2008 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+#ifndef _devODEManipulator_h
+#define _devODEManipulator_h
+
+#include <string>
+
+#include <cisstMultiTask/mtsTaskPeriodic.h>
+
+#include <cisstDevices/ode/devODEBody.h>
+#include <cisstDevices/ode/devODEJoint.h>
+#include <cisstDevices/ode/devODEWorld.h>
+#include <cisstDevices/devExport.h>
+
+class CISST_EXPORT devODEManipulator : public mtsTaskPeriodic {
+
+  //CMN_DECLARE_SERVICES( CMN_NO_DYNAMIC_CREATION , 10 );
+
+ private:
+
+  //! MTS vector of joint positions
+  mtsVector<double> jointspositions;
+
+  //! MTS function used to querry joints torques
+  mtsFunctionRead ReadFT;
+
+  //! A vector of joints
+  std::vector<devODEJoint*> joints;
+
+  vctDynamicVector<double> qinit;
+
+  //! Set the joint forces or torques
+  /**
+     This sets the force/torque value of each joint. This task will be applied
+     at each iteration of the world task. This method does NOT apply the FT 
+     values to each joint.
+   */
+  void SetForcesTorques( const vctDynamicVector<double>& ft);
+
+  //! Return the joints positions
+  /**
+     Query each ODE joint for their positions and add the initial offset to 
+     them
+     \return A vector of joints positions
+  */
+  vctDynamicVector<double> GetJointsPositions() const ;
+
+  //! Return the joints velocities
+  /**
+     Query each ODE joint for their velocities
+     \return A vector of joints velocities
+  */
+  vctDynamicVector<double> GetJointsVelocities() const ;
+
+
+ public: 
+
+  //! ODE Manipulator generic constructor
+  /**
+     This constructor initializes an ODE manipulator with the kinematics and 
+     dynamics contained in a file. Plus it initializes the ODE elements of the
+     manipulators (bodies and joints) for the engine.
+     \param devname The name of the task
+     \param period The period of the task
+     \param world The ODE world used by the manipulator
+     \param robotfilename The file with the kinematics and dynamics parameters
+     \param qinit The initial joint angles
+     \param Rtw0 The offset transformation of the robot base
+  */
+  devODEManipulator( const std::string& devname,
+		     double period,
+		     devODEWorld& world,
+		     const std::string& robotfilename,
+		     const vctDynamicVector<double> qinit,
+		     const std::vector<std::string>& geomfiles=std::vector<std::string>(),
+		     const vctFrame4x4<double>& Rtw0 = vctFrame4x4<double>() );
+
+  void Configure( const std::string& fn="");
+  void Startup();
+  void Run();
+  void Cleanup();
+
+  static const std::string PositionsInterface;
+  static const std::string ReadPositionsCommand;
+
+  static const std::string FTInterface;
+  static const std::string WriteFTCommand;
+
+};
+
+//CMN_DECLARE_SERVICES_INSTANTIATION( devODEManipulator );
+
+#endif
---
+++ libs/include/cisstDevices/ode/devODEWorld.h
@@ -0,0 +1,140 @@
+/*
+
+  Author(s): Simon Leonard
+  Created on: Nov 11 2009
+
+  (C) Copyright 2008 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+#ifndef _devODEWorld_h
+#define _devODEWorld_h
+
+#include <ode/ode.h>
+
+#include <cisstDevices/ode/devODEBody.h>
+#include <cisstDevices/ode/devODEJoint.h>
+
+#include <cisstMultiTask/mtsTaskPeriodic.h>
+#include <cisstVector/vctFixedSizeVector.h>
+#include <cisstDevices/devExport.h>
+
+class CISST_EXPORT devODEWorld : public mtsTaskPeriodic {
+
+  CMN_DECLARE_SERVICES( CMN_NO_DYNAMIC_CREATION , CMN_LOG_LOD_RUN_DEBUG );
+
+private:
+
+  //! The time step of the engine
+  double timestep;
+  
+  //! The ODE world ID
+  /**
+     In ODE, a world contain all the bodies and the spaces containing the 
+     geometries
+  */
+  dWorldID worldid;
+
+  //! The ODE space ID
+  /**
+     In ODE, a space contains all the geometries (shape) of the bodies. Spaces
+     are used to process collisions between geometries.
+  */
+  dSpaceID spaceid;
+
+  //! The floor geom
+  dGeomID floor;
+  
+  //! The ODE contact group
+  /**
+     Contact group is used to add contacts between geometries at each iteration.
+     This contact group is the one used by the world
+  */
+  dJointGroupID contactsgid;
+
+  //! Return the contacts group ID
+  /**
+     This is used internally for processing collisions. 
+     \return The contacts group ID.
+  */
+  dJointGroupID GroupID() const { return contactsgid; }
+
+  //! The maximum number of contacts
+  /**
+     This value determines the manimum number of contacts that can happen between
+     two bodies. Using several contact point slows down the simulation while too
+     few gives bad results (100 contacts points is actually quite large)
+  */
+  static const size_t NUM_CONTACTS = 100;
+
+  std::vector<devODEBody*> bodies;
+  std::vector<devODEJoint*> joints;
+
+public:
+
+  //! Create a new world
+  /**
+     Create a new ODE world. This initializes the ODE engine and create a new
+     world and a new top level space. It also sets simulation parameters such as
+     error reduction parameter (ERP) and constraint force mixing (CFM)
+     \param gravity A gravity vector. The defalt value is 
+                    \$\begin{bmatrix} 0 & 0 & -9.81 \end{bmatrix}\$.
+  */
+  devODEWorld( double period,
+	       const vctFixedSizeVector<double,3>& gravity = 
+	       vctFixedSizeVector<double,3>(0.0, 0.0, -9.81) );
+
+  //! Destroy the world!
+  ~devODEWorld();
+
+  //! Return the world ID
+  dWorldID WorldID() const { return worldid; }
+
+  //! Return the space ID
+  dSpaceID SpaceID() const { return spaceid; }
+
+  //! Detect a manipulator "self collision"
+  /**
+     A self collision is a collision when the manipulator hits itself. This is
+     called to avoid computing these self collision because they hog too much 
+     CPU and often make the simulation unstable. The problem is that adjacent 
+     links can touch each other at the joint, thus, creating permanent collision.
+     This really mess things up and it's better to avoid them altogether. Sure 
+     it less realistic but eventually it would be nice to have the manipulator 
+     handle these collision by themselves (and permit some links to interfere)
+     \param o1 The geometry of the first object
+     \param o2 The geometry of the second object
+     \return true if the two geometries belong to the same subspace. false
+             otherwise
+  */
+  bool SelfCollision( dGeomID o1, dGeomID o2 );
+
+  //! Process collisions between two geometries
+  /**
+     This is the main call to process collisions between geometries
+     \param o1 The geometry of the first object
+     \param o2 The geometry of the second object
+  */
+  void Collision( dGeomID o1, dGeomID o2 );
+
+  void Configure( const std::string& = "" ){}
+  void Startup(){}
+  void Cleanup(){}
+  void Run();
+
+  void Insert( devODEBody* body )  { bodies.push_back( body ); }
+  void Insert( devODEJoint* joint ) { joints.push_back( joint ); }
+
+};
+
+CMN_DECLARE_SERVICES_INSTANTIATION( devODEWorld );
+
+#endif
