c18688ce519ff241979660787b8bdd6a149652ee
Simon Leonard <sleonard@jhu.edu>
2010-04-29 20:03:35 +0000

    Added PD joint controller with gravity compensation.
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/main@1425 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ libs/code/cisstDevices/controllers/devPDGC.cpp
@@ -0,0 +1,41 @@
+#include <cisstDevices/controllers/devPDGC.h>
+
+devPDGC::devPDGC( const std::string& taskname, 
+		  double period,
+		  const std::string& robfile,
+		  const vctFrame4x4<double>& Rtw0,
+		  bool enabled,
+		  const vctDynamicMatrix<double>& Kp,
+		  const vctDynamicMatrix<double>& Kd ) :
+  
+  devControllerJoints( taskname, period, robfile, Rtw0, enabled ),
+  Kp( Kp ),
+  Kd( Kd ){
+
+  eold.SetSize( links.size() );
+  eold.SetAll( 0.0 );
+}
+
+vctDynamicVector<double> devPDGC::Control( const vctDynamicVector<double>& q, 
+					   const vctDynamicVector<double>&,
+					   double dt ){
+  
+  vctDynamicVector<double> qd( links.size(), 0.0 );
+  vctDynamicVector<double> qdd( links.size(), 0.0 );
+
+  // gravity compensation
+  vctDynamicVector<double> taug = InverseDynamics( q, qd, qdd );
+
+  // error = current - desired
+  vctDynamicVector<double> e = mtsqs - q;
+
+  // error time derivative
+  vctDynamicVector<double> ed = (e - eold)/dt;      
+
+  std::cout << e << std::endl;
+
+  eold = e;
+  //std::cout << Kd*ed + Kp*e + taug << std::endl;
+  return Kd*ed + Kp*e + taug;
+  
+}
