05c84d5d2efbaae7b0861b5a549dbfeb93736284
Min Yang Jung <myj@jhu.edu>
2010-09-16 15:30:01 +0000

    branches/main: updated CAN files
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/main@1805 0959eb1a-6956-4f60-947b-6092a4e22c97

--- libs/include/cisstDevices/can/CiA301.h
+++ libs/include/cisstDevices/can/CiA301.h
@@ -4,212 +4,213 @@
 
 #include <cisstDevices/can/devCAN.h>
 
-namespace CiA301{
+namespace CiA301 {
 
-  typedef devCAN::Frame::ID COBID;
+    typedef devCAN::Frame::ID COBID;
 
-  namespace Node
-  {
-    enum ID
-      {
-	NODE_0 = 0x000,
-	NODE_1 = 0x001,
-	NODE_2 = 0x002,
-	NODE_3 = 0x003,
-	NODE_4 = 0x004,
-	NODE_5 = 0x005,
-	NODE_6 = 0x006,
-      };
-    
-    CiA301::Node::ID operator++( CiA301::Node::ID& id, int  );
-
-  }
-  
-  namespace NMT
-  {
-    const devCAN::Frame::ID COBID   = 0x0000;
-    enum State
-      {
-	OPERATIONAL         = 0x01,
-	STOPPED             = 0x02,
-	PRE_OPERATIONAL     = 0x80,
-	RESET_NODE          = 0x81,
-	RESET_COMMUNICATION = 0x82
-      };
-  }
-  
-  namespace BOOTUP
-  {
-    namespace COBID{
-      const CiA301::COBID LOW  = 0x0700;
-      const CiA301::COBID HIGH = 0x077F;
-    }
-  }
-  
-  namespace SYNC
-  {
-    const devCAN::Frame::ID COBID = 0x0080;
-  }
-  
-  namespace EMERGENCY
-  {
-    namespace COBID
+    namespace Node
     {
-      const devCAN::Frame::ID LOW  = 0x0080;
-      const devCAN::Frame::ID HIGH = 0x00FF;
+        typedef unsigned int NodeType;
+
+        enum ID
+        {
+            NODE_0 = 0x000,
+            NODE_1 = 0x001,
+            NODE_2 = 0x002,
+            NODE_3 = 0x003,
+            NODE_4 = 0x004,
+            NODE_5 = 0x005,
+            NODE_6 = 0x006,
+        };
+
+        CiA301::Node::ID operator++( CiA301::Node::ID& id, int  );
     }
-    
-    enum Code
-      {
-	PVT_SEQUENCE_COUNTER  = 0x00,
-	PVT_CANNOT_BE_STARTED = 0x01,
-	PVT_BUFFER_UNDERFLOW  = 0x02,
-      };
-  }
-  
-  namespace TIMESTAMP
-  {
-    const devCAN::Frame::ID COBID = 0x0100;
-  }
-  
-  namespace PDO
-  {
 
-    namespace COBID
+    namespace NMT
     {
-      const devCAN::Frame::ID LOW  = 0x0180;
-      const devCAN::Frame::ID HIGH = 0x057F;
-    } // COBID
-
-    enum SubIndex { COMMUNICATION = 0x01, TRANSMISSION  = 0x02 };
-
-    enum Transmission 
-      { 
-	SYNC_ACYCLIC   = 0x00,
-	SYNC_CYCLIC    = 0x01,   // not really it's a range
-	SYNC_RTR       = 0xFC,  
-	ASYNC_RTR      = 0xFD,
-	ASYNC          = 0xFE
-      };
-  }
-  
-  namespace SDO
-  {
-    
-    namespace COBID
-    {
-      const devCAN::Frame::ID LOW  = 0x0600;
-      const devCAN::Frame::ID HIGH = 0x067F;
+        typedef unsigned char CommandType;
+        const devCAN::Frame::ID COBID = 0x0000;
+        enum State
+        {
+            OPERATIONAL         = 0x01,
+            STOPPED             = 0x02,
+            PRE_OPERATIONAL     = 0x80,
+            RESET_NODE          = 0x81,
+            RESET_COMMUNICATION = 0x82
+        };
     }
-    
-    enum Command
-      {
-	INITIATE_READ  = 0x40,
-	INITIATE_WRITE = 0x20
-      };
-    
-  }   
 
-  // An object has one index and one sub index
-  struct Object{
-    typedef unsigned short   Index;
-    typedef unsigned char SubIndex;
-    typedef int               Data;
-    CiA301::Object::Index       index;
-    CiA301::Object::SubIndex subindex;
-    CiA301::Object::Data         data;
-    Object( CiA301::Object::Index       idx, 
-	    CiA301::Object::SubIndex sidx=0, 
-	    CiA301::Object::Data     data=0 ) : 
-      index( idx ), subindex( sidx ), data( data ){}
-  };
+    namespace BOOTUP
+    {
+        namespace COBID{
+            const CiA301::COBID LOW  = 0x0700;
+            const CiA301::COBID HIGH = 0x077F;
+        }
+    }
 
-  struct RPDO2 : public CiA301::Object {
-    RPDO2( CiA301::COBID cobid )          : 
-      Object( 0x1401, CiA301::PDO::COMMUNICATION, cobid ){}
-    RPDO2( CiA301::PDO::Transmission transmission ) : 
-      Object( 0x1401, CiA301::PDO::TRANSMISSION, transmission ){}
-  };
+    namespace SYNC
+    {
+        const devCAN::Frame::ID COBID = 0x0080;
+    }
 
-  struct RPDO21 : public CiA301::Object{
-    RPDO21( CiA301::COBID cobid )          : 
-      Object( 0x1414, CiA301::PDO::COMMUNICATION, cobid ){}
-    RPDO21( CiA301::PDO::Transmission transmission ) : 
-      Object( 0x1414, CiA301::PDO::TRANSMISSION, transmission ){}
-  };
+    namespace EMERGENCY
+    {
+        namespace COBID
+        {
+            const devCAN::Frame::ID LOW  = 0x0080;
+            const devCAN::Frame::ID HIGH = 0x00FF;
+        }
 
-  struct RPDO24 : public CiA301::Object{
-    RPDO24( CiA301::COBID cobid )          : 
-      Object( 0x1417, CiA301::PDO::COMMUNICATION, cobid ){}
-    RPDO24( CiA301::PDO::Transmission transmission ) : 
-      Object( 0x1417, CiA301::PDO::TRANSMISSION, transmission ){}
-  };
+        enum Code
+        {
+            PVT_SEQUENCE_COUNTER  = 0x00,
+            PVT_CANNOT_BE_STARTED = 0x01,
+            PVT_BUFFER_UNDERFLOW  = 0x02,
+        };
+    }
 
-  struct TPDO1 : public CiA301::Object{
-    TPDO1( CiA301::COBID cobid )          : 
-      Object( 0x1800, CiA301::PDO::COMMUNICATION, cobid ){}
-    TPDO1( CiA301::PDO::Transmission transmission ) : 
-      Object( 0x1800, CiA301::PDO::TRANSMISSION, transmission ){}
-  };
+    namespace TIMESTAMP
+    {
+        const devCAN::Frame::ID COBID = 0x0100;
+    }
 
-  struct TPDO24 : public CiA301::Object{
-    TPDO24( CiA301::COBID cobid )          : 
-      Object( 0x1817, CiA301::PDO::COMMUNICATION, cobid ){}
-    TPDO24( CiA301::PDO::Transmission transmission ) : 
-      Object( 0x1817, CiA301::PDO::TRANSMISSION, transmission ){}
-  };
+    namespace PDO
+    {
+        namespace COBID
+        {
+            const devCAN::Frame::ID LOW  = 0x0180;
+            const devCAN::Frame::ID HIGH = 0x057F;
+        } // COBID
 
-  struct TPDO25 : public CiA301::Object{
-    TPDO25( CiA301::COBID cobid )          : 
-      Object( 0x1818, CiA301::PDO::COMMUNICATION, cobid ){}
-    TPDO25( CiA301::PDO::Transmission transmission ) : 
-      Object( 0x1818, CiA301::PDO::TRANSMISSION, transmission ){}
-  };
+        enum SubIndex { COMMUNICATION = 0x01, TRANSMISSION  = 0x02 };
 
-  struct TPDO26 : public CiA301::Object{
-    enum Mapping
-      { 
-	MAPPING_CNT = 0x00,
-	MAPPING_1   = 0x01, 
-	MAPPING_2   = 0x02, 
-	MAPPING_3   = 0x03, 
-	MAPPING_4   = 0x04,
-	MAPPING_5   = 0x05,
-      };
-    TPDO26( CiA301::COBID cobid )          : 
-      Object( 0x1819, CiA301::PDO::COMMUNICATION, cobid ){}
-    TPDO26( CiA301::PDO::Transmission transmission ) : 
-      Object( 0x1819, CiA301::PDO::TRANSMISSION, transmission ){}
-    TPDO26( CiA301::TPDO26::Mapping mapping,  CiA301::TPDO26::Data data ) :
-      Object( 0x1A19, mapping, data ){} 
-  };
+        enum Transmission 
+        { 
+            SYNC_ACYCLIC   = 0x00,
+            SYNC_CYCLIC    = 0x01,   // not really it's a range
+            SYNC_RTR       = 0xFC,  
+            ASYNC_RTR      = 0xFD,
+            ASYNC          = 0xFE
+        };
+    }
 
-  
+    namespace SDO
+    {
+        typedef unsigned char CommandType;
 
-  
-  /*
-      namespace TPDO26
-      {
-	enum Index
-	  {
-	    MAPPING          = 0x1A19
-	  } // 
+        namespace COBID
+        {
+            const devCAN::Frame::ID LOW  = 0x0600;
+            const devCAN::Frame::ID HIGH = 0x067F;
+        }
 
-	enum Mapping
-	  {
-	    COUNT         = 0x00;
-	    MAPPING_1     = 0x01;
-	    MAPPING_2     = 0x02;
-	    MAPPING_3     = 0x03;
-	    MAPPING_4     = 0x04;
-	    MAPPING_5     = 0x05;
-	    MAPPING_6     = 0x06;
-	    MAPPING_7     = 0x07;
-	    MAPPING_8     = 0x08;
-	  } // mapping
+        enum Command
+        {
+            INITIATE_READ  = 0x40,
+            INITIATE_WRITE = 0x20
+        };
+    }   
 
-      } // TPDO26
-    */      
+    // An object has one index and one sub index
+    struct Object{
+        typedef unsigned short   Index;
+        typedef unsigned char SubIndex;
+        typedef int               Data;
+        CiA301::Object::Index       index;
+        CiA301::Object::SubIndex subindex;
+        CiA301::Object::Data         data;
+        Object( CiA301::Object::Index       idx, 
+                CiA301::Object::SubIndex sidx=0, 
+                CiA301::Object::Data     data=0 ) : 
+            index( idx ), subindex( sidx ), data( data ){}
+    };
+
+    struct RPDO2 : public CiA301::Object {
+        RPDO2( CiA301::COBID cobid )          : 
+            Object( 0x1401, CiA301::PDO::COMMUNICATION, cobid ){}
+        RPDO2( CiA301::PDO::Transmission transmission ) : 
+            Object( 0x1401, CiA301::PDO::TRANSMISSION, transmission ){}
+    };
+
+    struct RPDO21 : public CiA301::Object{
+        RPDO21( CiA301::COBID cobid )          : 
+            Object( 0x1414, CiA301::PDO::COMMUNICATION, cobid ){}
+        RPDO21( CiA301::PDO::Transmission transmission ) : 
+            Object( 0x1414, CiA301::PDO::TRANSMISSION, transmission ){}
+    };
+
+    struct RPDO24 : public CiA301::Object{
+        RPDO24( CiA301::COBID cobid )          : 
+            Object( 0x1417, CiA301::PDO::COMMUNICATION, cobid ){}
+        RPDO24( CiA301::PDO::Transmission transmission ) : 
+            Object( 0x1417, CiA301::PDO::TRANSMISSION, transmission ){}
+    };
+
+    struct TPDO1 : public CiA301::Object{
+        TPDO1( CiA301::COBID cobid )          : 
+            Object( 0x1800, CiA301::PDO::COMMUNICATION, cobid ){}
+        TPDO1( CiA301::PDO::Transmission transmission ) : 
+            Object( 0x1800, CiA301::PDO::TRANSMISSION, transmission ){}
+    };
+
+    struct TPDO24 : public CiA301::Object{
+        TPDO24( CiA301::COBID cobid )          : 
+            Object( 0x1817, CiA301::PDO::COMMUNICATION, cobid ){}
+        TPDO24( CiA301::PDO::Transmission transmission ) : 
+            Object( 0x1817, CiA301::PDO::TRANSMISSION, transmission ){}
+    };
+
+    struct TPDO25 : public CiA301::Object{
+        TPDO25( CiA301::COBID cobid )          : 
+            Object( 0x1818, CiA301::PDO::COMMUNICATION, cobid ){}
+        TPDO25( CiA301::PDO::Transmission transmission ) : 
+            Object( 0x1818, CiA301::PDO::TRANSMISSION, transmission ){}
+    };
+
+    struct TPDO26 : public CiA301::Object{
+        enum Mapping
+        { 
+            MAPPING_CNT = 0x00,
+            MAPPING_1   = 0x01, 
+            MAPPING_2   = 0x02, 
+            MAPPING_3   = 0x03, 
+            MAPPING_4   = 0x04,
+            MAPPING_5   = 0x05,
+        };
+        TPDO26( CiA301::COBID cobid )          : 
+            Object( 0x1819, CiA301::PDO::COMMUNICATION, cobid ){}
+        TPDO26( CiA301::PDO::Transmission transmission ) : 
+            Object( 0x1819, CiA301::PDO::TRANSMISSION, transmission ){}
+        TPDO26( CiA301::TPDO26::Mapping mapping,  CiA301::TPDO26::Data data ) :
+            Object( 0x1A19, mapping, data ){} 
+    };
+
+
+
+
+    /*
+       namespace TPDO26
+       {
+       enum Index
+       {
+       MAPPING          = 0x1A19
+       } // 
+
+       enum Mapping
+       {
+       COUNT         = 0x00;
+       MAPPING_1     = 0x01;
+       MAPPING_2     = 0x02;
+       MAPPING_3     = 0x03;
+       MAPPING_4     = 0x04;
+       MAPPING_5     = 0x05;
+       MAPPING_6     = 0x06;
+       MAPPING_7     = 0x07;
+       MAPPING_8     = 0x08;
+       } // mapping
+
+       } // TPDO26
+     */      
 }
 
 #endif
--- libs/include/cisstDevices/can/CiA402.h
+++ libs/include/cisstDevices/can/CiA402.h
@@ -6,94 +6,94 @@
 
 namespace CiA402{
 
-  struct ControlWord : public CiA301::Object {
-    enum Word
-      {
-	RESET_FAULT      = 0x80,
-	DISABLE_VOLTAGE  = 0x04,
-	SHUTDOWN         = 0x06,
-	SWITCH_ON        = 0x07,
-	ENABLE_OPERATION = 0x0F,
-	QUICK_STOP       = 0x02,
-	START_HOMING     = 0x1F,
-	END_HOMING       = 0x0F
-      }; // Control Word
-    ControlWord( ControlWord::Word word ) : 
-      CiA301::Object( 0x6040, 0, word ){}
-  };
-  
-  struct OperationMode : public CiA301::Object {
-    enum Mode
-      {	
-	POSITION = 1,
-	VELOCITY = 3,
-	TORQUE   = 4,
-	HOMING   = 6,
-	PVT      = 7,
-	CUSTOM   = 0xFF
-      };
-    OperationMode( OperationMode::Mode mode ) :
-      CiA301::Object( 0x6060, 0, mode ){}
-  };
-  
-  struct ActualPosition : public CiA301::Object{
-    ActualPosition() : 
-      CiA301::Object( 0x6064 ){}
-  };
+    struct ControlWord : public CiA301::Object {
+        enum Word
+        {
+            RESET_FAULT      = 0x80,
+            DISABLE_VOLTAGE  = 0x04,
+            SHUTDOWN         = 0x06,
+            SWITCH_ON        = 0x07,
+            ENABLE_OPERATION = 0x0F,
+            QUICK_STOP       = 0x02,
+            START_HOMING     = 0x1F,
+            END_HOMING       = 0x0F
+        }; // Control Word
+        ControlWord( ControlWord::Word word ) : 
+            CiA301::Object( 0x6040, 0, word ){}
+    };
 
-   
-   // Homing stuff
-  struct HomeOffset : public CiA301::Object {
-    HomeOffset( int offset ) : 
-      CiA301::Object( 0x607C, 0, offset ){}
-  };
+    struct OperationMode : public CiA301::Object {
+        typedef unsigned char Type;
+        enum Mode
+        {	
+            POSITION = 1,
+            VELOCITY = 3,
+            TORQUE   = 4,
+            HOMING   = 6,
+            PVT      = 7,
+            CUSTOM   = 0xFF
+        };
+        OperationMode( OperationMode::Mode mode ) :
+            CiA301::Object( 0x6060, 0, mode ){}
+    };
 
-  struct HomingMethod : public CiA301::Object {
-    HomingMethod( int method ) : 
-      CiA301::Object( 0x6098, 0, method ){}
-  };
-
-  struct HomingSpeed : public CiA301::Object {
-    enum Search{ SWITCH = 0x01, ZERO = 0x02 };
-    HomingSpeed( HomingSpeed::Search search, unsigned int speed ) : 
-      CiA301::Object( 0x6099, search, speed ){}
-  };
-
-  struct HomingAcceleration : public CiA301::Object {
-    HomingAcceleration( int acceleration ) : 
-      CiA301::Object( 0x609A, 0, acceleration ){}
-  };
-
-   
-  // PVT stuff 
-  struct PVTClearBuffer : public CiA301::Object{
-    PVTClearBuffer() : 
-    CiA301::Object( 0x60C4, 6, 0 ){}
-  };
-
-   
-   /*
-  enum Index
-    {
-      HOMING_ACCELERATION      = 0x609A,
-      PVT_BUFFER_CONFIGURATION = 0x60C4
-    };  // Index
-  typedef unsigned short SubIndex;
-  */
+    struct ActualPosition : public CiA301::Object{
+        ActualPosition() : 
+            CiA301::Object( 0x6064 ){}
+    };
 
 
-  /*
-  namespace PVTBufferConfig
-  {
-    const SubIndex MAX_BUFFER_SIZE     = 0x01;
-    const SubIndex ACTUAL_BUFFER_SIZE  = 0x02;
-    const SubIndex BUFFER_ORGANIZATION = 0x03;
-    const SubIndex BUFFER_POSITION     = 0x04;
-    const SubIndex DATA_RECORD_SIZE    = 0x05;
-    const SubIndex BUFFER_CLEAR        = 0x06;
-  } // PVT buffer
-  */
+    // Homing stuff
+    struct HomeOffset : public CiA301::Object {
+        HomeOffset( int offset ) : 
+            CiA301::Object( 0x607C, 0, offset ){}
+    };
 
+    struct HomingMethod : public CiA301::Object {
+        HomingMethod( int method ) : 
+            CiA301::Object( 0x6098, 0, method ){}
+    };
+
+    struct HomingSpeed : public CiA301::Object {
+        enum Search{ SWITCH = 0x01, ZERO = 0x02 };
+        HomingSpeed( HomingSpeed::Search search, unsigned int speed ) : 
+            CiA301::Object( 0x6099, search, speed ){}
+    };
+
+    struct HomingAcceleration : public CiA301::Object {
+        HomingAcceleration( int acceleration ) : 
+            CiA301::Object( 0x609A, 0, acceleration ){}
+    };
+
+
+    // PVT stuff 
+    struct PVTClearBuffer : public CiA301::Object{
+        PVTClearBuffer() : 
+            CiA301::Object( 0x60C4, 6, 0 ){}
+    };
+
+
+    /*
+       enum Index
+       {
+       HOMING_ACCELERATION      = 0x609A,
+       PVT_BUFFER_CONFIGURATION = 0x60C4
+       };  // Index
+       typedef unsigned short SubIndex;
+     */
+
+
+    /*
+       namespace PVTBufferConfig
+       {
+       const SubIndex MAX_BUFFER_SIZE     = 0x01;
+       const SubIndex ACTUAL_BUFFER_SIZE  = 0x02;
+       const SubIndex BUFFER_ORGANIZATION = 0x03;
+       const SubIndex BUFFER_POSITION     = 0x04;
+       const SubIndex DATA_RECORD_SIZE    = 0x05;
+       const SubIndex BUFFER_CLEAR        = 0x06;
+       } // PVT buffer
+     */
 }
 
 #endif
