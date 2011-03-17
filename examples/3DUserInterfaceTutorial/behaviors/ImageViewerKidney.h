7535854074d938b7a3cfd30a392aba321e3f8c1a
Anton Deguet <anton.deguet@jhu.edu>
2011-03-17 01:08:57 +0000

    /trunk, 3DUserInterface tutorial: dos2unix and missing svn Id keyword.
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@2394 0959eb1a-6956-4f60-947b-6092a4e22c97

--- examples/3DUserInterfaceTutorial/behaviors/ImageViewerKidney.h
+++ examples/3DUserInterfaceTutorial/behaviors/ImageViewerKidney.h
@@ -2,7 +2,7 @@
 /* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
 
 /*
-  $Id: ImageViewerKidney.h 848 2009-09-13 17:59:39Z adeguet1 $
+  $Id$
 
   Author(s):	Anton Deguet, Simon DiMaio
   Created on:	2009-09-13
