726b1d626b061d3c7b9955dc4f5828c4df11ab5e
Balazs Vagvolgyi <balazs.vagvolgyi@jhu.edu>
2011-04-19 15:59:18 +0000

    stereoTutorial: comments added
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@2462 0959eb1a-6956-4f60-947b-6092a4e22c97

--- examples/stereoTutorial/tutorial3/main.cpp
+++ examples/stereoTutorial/tutorial3/main.cpp
@@ -7,26 +7,26 @@
 
 int main()
 {
-    svlInitialize();                                 // 1. Discover supported devices and codecs
-
-    svlStreamManager stream;                         // 2. Instantiate SVL Stream
-    svlFilterSourceVideoCapture source(1);           // 3. Instantiate video capture filter
-    CMyFilter1 filter1;                              // 4. Instantiate user-implemented filter #1
-    svlFilterImageWindow window;                     // 5. Instantiate image window filter
-
-    source.SetDevice(0);                             // 6. Select first available capture device
-
-    stream.SetSourceFilter(&source);                 // 7. Assign source filter to stream
-    source.GetOutput()->Connect(filter1.GetInput()); // 8. Connect source filter to filter #1
-    filter1.GetOutput()->Connect(window.GetInput()); // 9. Connect filter #1 to window filter
-
-    stream.Play();                                   // 10. Initialize and Play video stream
-
-    char ch;
+    svlInitialize();                                 // Discover supported devices and codecs
+                                                     //
+    svlStreamManager stream;                         // Instantiate SVL Stream
+    svlFilterSourceVideoCapture source(1);           // Instantiate video capture filter
+    CMyFilter1 filter1;                              // Instantiate user-implemented filter #1
+    svlFilterImageWindow window;                     // Instantiate image window filter
+                                                     //
+    source.SetDevice(0);                             // Select first available capture device
+                                                     //
+    stream.SetSourceFilter(&source);                 // Assign source filter to stream
+    source.GetOutput()->Connect(filter1.GetInput()); // Connect source filter to filter #1
+    filter1.GetOutput()->Connect(window.GetInput()); // Connect filter #1 to window filter
+                                                     //
+    stream.Play();                                   // Initialize and Play video stream
+                                                     //
+    char ch;                                         //
     std::cin >> ch;                                  // (Wait for key-press)
-
-    stream.Release();                                // 11. Release stream
-
-    return 0;
+                                                     //
+    stream.Release();                                // Release stream
+                                                     //
+    return 0;                                        //
 }
 
