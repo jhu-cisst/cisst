2fd45540ff4e33df1c3c8634c059d9374789851f
Balazs Vagvolgyi <balazs.vagvolgyi@jhu.edu>
2011-05-03 15:42:05 +0000

    /trunk: stereoTutorial - tutorial #2 updated
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@2515 0959eb1a-6956-4f60-947b-6092a4e22c97

--- examples/stereoTutorial/tutorial2/main.cpp
+++ examples/stereoTutorial/tutorial2/main.cpp
@@ -7,26 +7,26 @@
 
 int main()
 {
-    svlInitialize();                                // 1. Discover supported devices and codecs
+    svlInitialize();                                // Discover supported devices and codecs
 
-    svlStreamManager stream;                        // 2. Instantiate SVL Stream
-    svlFilterSourceVideoCapture source(1);          // 3. Instantiate video capture filter
-    svlFilterImageWindow window;                    // 4. Instantiate image window filter
+    svlStreamManager stream;                        // Instantiate SVL Stream
+    svlFilterSourceVideoCapture source(1);          // Instantiate video capture filter
+    svlFilterImageWindow window;                    // Instantiate image window filter
 
-    CMyEventHandler event_handler;                  // 5. Instantiate our own window event handler
-    window.SetEventHandler(&event_handler);         // 6. Assign event handler to image window filter
+    CMyEventHandler event_handler;                  // Instantiate our own window event handler
+    window.SetEventHandler(&event_handler);         // Assign event handler to image window filter
 
-    source.SetDevice(0);                            // 7. Select first available capture device
+    source.SetDevice(0);                            // Select first available capture device
 
-    stream.SetSourceFilter(&source);                // 8. Assign source filter to stream
-    source.GetOutput()->Connect(window.GetInput()); // 9. Connect source filter to window filter
+    stream.SetSourceFilter(&source);                // Assign source filter to stream
+    source.GetOutput()->Connect(window.GetInput()); // Connect source filter to window filter
 
-    stream.Play();                                  // 10. Initialize and Play video stream
+    stream.Play();                                  // Initialize and Play video stream
 
     char ch;
     std::cin >> ch;                                 // (Wait for key-press)
 
-    stream.Release();                               // 11. Release stream
+    stream.Release();                               // Release stream
 
     return 0;
 }
