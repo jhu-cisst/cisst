8cd575ab7ce7d760b785f588bcca3215963092bb
Balazs Vagvolgyi <balazs.vagvolgyi@jhu.edu>
2011-05-17 15:46:12 +0000

    /trunk: tutorial5 added to stereoTutorial
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@2561 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ examples/stereoTutorial/tutorial5/CMyEventHandler2.h
@@ -0,0 +1,25 @@
+// See license at http://www.cisst.org/cisst/license.txt
+
+
+#ifndef _CMyEventHandler2_h
+#define _CMyEventHandler2_h
+
+#include <cisstStereoVision/svlFilterImageWindow.h>
+#include <cisstStereoVision/svlOverlayObjects.h>
+
+
+class CMyEventHandler2 : public svlWindowEventHandlerBase
+{
+public:
+    CMyEventHandler2();
+
+    void OnUserEvent(unsigned int winid, bool ascii, unsigned int eventid);
+
+    svlOverlayStaticText* TextBox;
+    svlOverlayStaticPoly* Path;
+    bool LMouseButtonPressed;
+};
+
+
+#endif // _CMyEventHandler2_h
+
