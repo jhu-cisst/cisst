0e02304bc51cb3c7860fb94ee61c8fa1138c288a
Joshua Chuang <chuang.joshua@gmail.com>
2011-05-18 01:34:34 +0000

    /branches/data-player: Modify array size to smaller one and refine naming
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/2011-02-25-data-player@2566 0959eb1a-6956-4f60-947b-6092a4e22c97

--- tests/cisstVector/vctPlot2DBaseTest.cpp
+++ tests/cisstVector/vctPlot2DBaseTest.cpp
@@ -4,109 +4,108 @@ void vctPlot2DBaseTest::TestBufferManipulating(void) {
 
     vctPlot2DBaseTestClass Plot;
     vctPlot2DBaseTestClass::Trace * trace = Plot.AddTrace("TestTrace");
-    const int TestRange = 10000000;
-    //double ArrayToAppend[TestRange*2];
-    //double ArrayToPrepend[TestRange*2];
-    double *ArrayToAppend = new double [TestRange*2];
-    double *ArrayToPrepend = new double [TestRange*2];
+    const int DataElements= 100;    
+    const int PointSize = 2;
+    double *ArrayToAppend = new double [DataElements*PointSize];
+    double *ArrayToPrepend = new double [DataElements*PointSize];
     size_t NumberOfPoint, BufferSize;
 
      int i,j;
-
+         
      // 100~199, append array
     j = 0;
-    for( i = 0; i < TestRange*2; i+=2){
-        ArrayToAppend[i] = TestRange+j;
-        ArrayToAppend[i+1] = TestRange+j;
+    for( i = 0; i < DataElements*PointSize; i+=PointSize){
+        ArrayToAppend[i] = DataElements+j;
+        ArrayToAppend[i+1] = DataElements+j;
         j++;
     }
 
      // -100 ~ -1, prepend array
-     j = 0-TestRange;
-    for( i = 0; i < TestRange*2; i+=2){
+     j = 0-DataElements;
+    for( i = 0; i < DataElements*PointSize; i+=PointSize){
         ArrayToPrepend[i] = j;
         ArrayToPrepend[i+1] = j;
         j++;
     }
 
     /****** TEST AddPoint() ******/
-    trace->SetSize(TestRange);
+    trace->SetSize(DataElements);
     // 0 ~ 99
-     for(double i = 0; i < TestRange; i++)
+     for(double i = 0; i < DataElements; i++)
         trace->AddPoint(vctDouble2(i,i));
 
-     for( i =0 ; i < TestRange; i++){
+     for( i =0 ; i < DataElements; i++){
         vctDouble2 Point = trace->GetPointAt(i);        
         CPPUNIT_ASSERT(Point.X() == i) ;
         CPPUNIT_ASSERT(Point.Y() == i) ;
      }
 
     /****** TEST ReSize(), Expand ******/
-    trace->ReSize(TestRange*2);
+    trace->ReSize(DataElements*2);
     trace->GetNumberOfPoints(NumberOfPoint, BufferSize);
-    CPPUNIT_ASSERT(NumberOfPoint == TestRange);
-    CPPUNIT_ASSERT(BufferSize == TestRange*2);
+    CPPUNIT_ASSERT(NumberOfPoint == DataElements);
+    CPPUNIT_ASSERT(BufferSize == DataElements*2);
     // 0 ~ 99, After expanding
-     for(int i =0 ; i < TestRange; i++){
+     for(int i =0 ; i < DataElements; i++){
         vctDouble2 Point = trace->GetPointAt(i);        
         CPPUNIT_ASSERT(Point.X() == i) ;
         CPPUNIT_ASSERT(Point.Y() == i) ;
      }
 
      /****** TEST AppendArray() ******/
-     CPPUNIT_ASSERT(trace->AppendArray(ArrayToAppend, TestRange*2));
+     CPPUNIT_ASSERT(trace->AppendArray(ArrayToAppend, DataElements*PointSize));
     
      // 0~199, After append
-     for( i =0 ; i < TestRange*2; i++){
+     for( i =0 ; i < DataElements*2; i++){
         vctDouble2 Point = trace->GetPointAt(i);        
         CPPUNIT_ASSERT(Point.X() == i) ;
         CPPUNIT_ASSERT(Point.Y() == i) ;
      }
 
      /****** TEST ReSize(), Shrink ******/
-     trace->ReSize(TestRange);
+     trace->ReSize(DataElements);
      trace->GetNumberOfPoints(NumberOfPoint, BufferSize);
-     CPPUNIT_ASSERT(NumberOfPoint == TestRange);
-     CPPUNIT_ASSERT(BufferSize == TestRange);
+     CPPUNIT_ASSERT(NumberOfPoint == DataElements);
+     CPPUNIT_ASSERT(BufferSize == DataElements);
 
     // 0~99, After shrinking
-     for( i =0 ; i < TestRange; i++){
+     for( i =0 ; i < DataElements; i++){
         vctDouble2 Point = trace->GetPointAt(i);
         CPPUNIT_ASSERT(Point.X() == i) ;
         CPPUNIT_ASSERT(Point.Y() == i) ;
      }
 
     /****** TEST Prepend() ******/
-    trace->ReSize(TestRange*2);
-    trace->PrependArray(ArrayToPrepend, TestRange*2);
+    trace->ReSize(DataElements*2);
+    trace->PrependArray(ArrayToPrepend, DataElements*PointSize);
     trace->GetNumberOfPoints(NumberOfPoint, BufferSize);
-    CPPUNIT_ASSERT(NumberOfPoint == TestRange*2);
-    CPPUNIT_ASSERT(BufferSize == TestRange*2);
+    CPPUNIT_ASSERT(NumberOfPoint == DataElements*2);
+    CPPUNIT_ASSERT(BufferSize == DataElements*2);
     // -100 ~ 99
-    for( i =0 ; i < TestRange*2; i++){
+    for( i =0 ; i < DataElements*2; i++){
         vctDouble2 Point = trace->GetPointAt(i);        
-        CPPUNIT_ASSERT(Point.X() == (i-TestRange)) ;
-        CPPUNIT_ASSERT(Point.Y() == (i-TestRange)) ;        
+        CPPUNIT_ASSERT(Point.X() == (i-DataElements)) ;
+        CPPUNIT_ASSERT(Point.Y() == (i-DataElements)) ;        
     }
 
      /****** TEST SetArrayAt() ******/
-    trace->SetArrayAt(0, ArrayToPrepend, TestRange*2);
+    trace->SetArrayAt(0, ArrayToPrepend, DataElements*PointSize);
 
     // -100 ~ 99
-    for( i =0 ; i < TestRange*2; i++){
+    for( i =0 ; i < DataElements*2; i++){
         vctDouble2 Point = trace->GetPointAt(i);
-        CPPUNIT_ASSERT(Point.X() == (i-TestRange)) ;
-        CPPUNIT_ASSERT(Point.Y() == (i-TestRange)) ;
+        CPPUNIT_ASSERT(Point.X() == (i-DataElements)) ;
+        CPPUNIT_ASSERT(Point.Y() == (i-DataElements)) ;
     }
 
      /****** TEST SetPointAt() ******/
     // -100 ~ 99
-    for( i =0 ; i < TestRange*2; i++){
+    for( i =0 ; i < DataElements*2; i++){
         vctDouble2 Point = trace->GetPointAt(i);
         trace->SetPointAt(i, Point);
         Point = trace->GetPointAt(i);
-        CPPUNIT_ASSERT(Point.X() == (i-TestRange)) ;
-        CPPUNIT_ASSERT(Point.Y() == (i-TestRange)) ;
+        CPPUNIT_ASSERT(Point.X() == (i-DataElements)) ;
+        CPPUNIT_ASSERT(Point.Y() == (i-DataElements)) ;
     }
     delete ArrayToAppend;
     delete ArrayToPrepend;
--- tests/cisstVector/vctPlot2DBaseTest.h
+++ tests/cisstVector/vctPlot2DBaseTest.h
@@ -3,22 +3,18 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include <cisstVector/vctPlot2DBase.h>
-// Always include last
-#include <cisstVector/vctExport.h>
 
 class vctPlot2DBaseTestClass: public vctPlot2DBase
-{
-
+{    
     virtual void RenderInitialize(void){};
     virtual void RenderResize(double width, double height){};
     virtual void Render(void){};
 };
 
 class vctPlot2DBaseTest : public CppUnit::TestFixture
-{
+{        
     CPPUNIT_TEST_SUITE(vctPlot2DBaseTest);
-    CPPUNIT_TEST(TestBufferManipulating);
-    //CPPUNIT_TEST(TestCrossProduct);
+    CPPUNIT_TEST(TestBufferManipulating);    
     CPPUNIT_TEST_SUITE_END();
     
  public:
@@ -28,11 +24,8 @@ class vctPlot2DBaseTest : public CppUnit::TestFixture
     void tearDown(void) {
     }
     
-    /*! Test the X(), Y() and Z() methods */
+    /*! Test the Prepend, Append, SetPoint methods */
     void TestBufferManipulating(void);
-
-    ///*! Test the cross product method */
-    //void TestCrossProduct(void);
 };
 
 
