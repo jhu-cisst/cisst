3d3f22fd0b0e711a45f4829ad5d32b31bb5eda54
Anton Deguet <anton.deguet@jhu.edu>
2011-06-11 04:08:21 +0000

    /trunk, osaCriticalSection: moved declaration of Windows critical section object to implementation file to avoid #include <windows.h> in header file.  Added unit tests for osaCriticalSection
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@2675 0959eb1a-6956-4f60-947b-6092a4e22c97

--- libs/include/cisstOSAbstraction/osaCriticalSection.h
+++ libs/include/cisstOSAbstraction/osaCriticalSection.h
@@ -3,11 +3,11 @@
 
 /*
   $Id$
-  
-  Author(s):  Balazs Vagvolgyi, Min Yang Jung
-  Created on: 2008 
 
-  (C) Copyright 2006-2009 Johns Hopkins University (JHU), All Rights
+  Author(s):  Balazs Vagvolgyi, Min Yang Jung
+  Created on: 2008
+
+  (C) Copyright 2006-2011 Johns Hopkins University (JHU), All Rights
   Reserved.
 
 --- begin cisst license - do not edit ---
@@ -23,39 +23,42 @@ http://www.cisst.org/cisst/license.txt.
 #ifndef _osaCriticalSection_h
 #define _osaCriticalSection_h
 
+
 #include <cisstCommon/cmnPortability.h>
 
-#if (CISST_OS == CISST_WINDOWS)
-    #ifndef _WIN32_WINNT
-        #define _WIN32_WINNT 0x0400
-    #endif // _WIN32_WINNT
-    #include <windows.h>
+#if (CISST_OS == CISST_LINUX_RTAI) || (CISST_OS == CISST_LINUX) || (CISST_OS == CISST_DARWIN) || (CISST_OS == CISST_SOLARIS) || (CISST_OS == CISST_QNX) || (CISST_OS == CISST_LINUX_XENOMAI)
+#include <cisstOSAbstraction/osaMutex.h>
 #endif
 
-#if (CISST_OS == CISST_LINUX_RTAI) || (CISST_OS == CISST_LINUX) || (CISST_OS == CISST_DARWIN) || (CISST_OS == CISST_SOLARIS) || (CISST_OS == CISST_QNX) || (CISST_OS == CISST_LINUX_XENOMAI)
-    #include <cisstOSAbstraction/osaMutex.h>
-#endif
 
 // Always include last
 #include <cisstOSAbstraction/osaExport.h>
 
 class CISST_EXPORT osaCriticalSection
 {
-public:
+    friend class osaMutexTest;
+
+#if (CISST_OS == CISST_WINDOWS)
+    // for Windows only, hide critical section handle in internals
+    enum {INTERNALS_SIZE = 32};
+    char Internals[INTERNALS_SIZE];
+#endif
+#if (CISST_OS == CISST_LINUX_RTAI) || (CISST_OS == CISST_LINUX) || (CISST_OS == CISST_DARWIN) || (CISST_OS == CISST_SOLARIS) || (CISST_OS == CISST_QNX) || (CISST_OS == CISST_LINUX_XENOMAI)
+    enum {INTERNALS_SIZE = 0};
+    osaMutex Mutex;
+#endif
+
+    /*! Return the size of the actual object used by the OS.  This is
+      used for testing only. */
+    static unsigned int SizeOfInternals(void);
+
+ public:
     osaCriticalSection();
     ~osaCriticalSection();
 
-    bool TryEnter();
-    void Enter();
-    void Leave();
-
-private:
-#if (CISST_OS == CISST_WINDOWS)
-    CRITICAL_SECTION csHandle;
-#endif
-#if (CISST_OS == CISST_LINUX_RTAI) || (CISST_OS == CISST_LINUX) || (CISST_OS == CISST_DARWIN) || (CISST_OS == CISST_SOLARIS) || (CISST_OS == CISST_QNX) || (CISST_OS == CISST_LINUX_XENOMAI)
-    osaMutex Mutex;
-#endif
+    bool TryEnter(void);
+    void Enter(void);
+    void Leave(void);
 };
 
 #endif // _osaCriticalSection_h
