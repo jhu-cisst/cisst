f0bccdd80b47cd219fe70755177727136d9f4049
Anton Deguet <anton.deguet@jhu.edu>
2011-09-26 02:20:21 +0000

    /branches/cmake: - deprecated CMake macro now send an error message - fixed SWIG wrapping in unit tests - added config files for Qt and FLTK (to ease find_package in external projects)
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/2011-09-22-cmake@2964 0959eb1a-6956-4f60-947b-6092a4e22c97

--- libs/cmake/cisstMacros.cmake
+++ libs/cmake/cisstMacros.cmake
@@ -31,7 +31,7 @@ macro (cisst_load_package_setting ...)
   cisst_cmake_debug ("cisst_load_package_setting called with: ${ARGV}")
   # Set all variables based on dependencies
   foreach (lib ${ARGV})
-    # Additional settings 
+    # Additional settings
     set (_clps_ADDITIONAL_BUILD_CMAKE "${CISST_BINARY_DIR}/libs/${lib}Build.cmake")
     if (EXISTS ${_clps_ADDITIONAL_BUILD_CMAKE})
       include (${_clps_ADDITIONAL_BUILD_CMAKE})
@@ -605,113 +605,18 @@ endfunction (cisst_data_generator)
 # -- add the library
 # -- create the install targets for the headers as well as the library
 
-MACRO(CISST_ADD_LIBRARY_TO_PROJECT PROJECT_NAME)
-
-# Make sure this lib should be compiled
-IF(CISST_BUILD_LIBS_${LIBRARY} OR BUILD_${LIBRARY})
-
-  # Build source list with full path
-  FOREACH(file ${SOURCE_FILES})
-    SET(SOURCES ${SOURCES} ${${PROJECT_NAME}_SOURCE_DIR}/code/${LIBRARY}/${file})
-  ENDFOREACH(file)
-
-
-  # Build header list with full path and generate a main header file for the library
-  STRING(ASCII 35 CISST_STRING_POUND)
-  SET(LIBRARY_MAIN_HEADER ${${PROJECT_NAME}_BINARY_DIR}/include/${LIBRARY}.h)
-  SET(LIBRARY_MAIN_HEADER_TMP ${${PROJECT_NAME}_BINARY_DIR}/include/${LIBRARY}.h.tmp)
-
-  SET(FILE_CONTENT "/* This file is generated automatically by CMake, DO NOT EDIT\n")
-  SET(FILE_CONTENT ${FILE_CONTENT} "   CMake: ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}\n")
-  SET(FILE_CONTENT ${FILE_CONTENT} "   System: ${CMAKE_SYSTEM}\n")
-  SET(FILE_CONTENT ${FILE_CONTENT} "   Source: ${CMAKE_SOURCE_DIR} */\n\n")
-  SET(FILE_CONTENT ${FILE_CONTENT} "${CISST_STRING_POUND}pragma once\n")
-  SET(FILE_CONTENT ${FILE_CONTENT} "${CISST_STRING_POUND}ifndef _${LIBRARY}_h\n")
-  SET(FILE_CONTENT ${FILE_CONTENT} "${CISST_STRING_POUND}define _${LIBRARY}_h\n\n")
-  FOREACH(file ${HEADER_FILES})
-    SET(HEADERS ${HEADERS} ${${PROJECT_NAME}_SOURCE_DIR}/include/${LIBRARY}/${file})
-    SET(FILE_CONTENT ${FILE_CONTENT} "${CISST_STRING_POUND}include <${LIBRARY}/${file}>\n")
-  ENDFOREACH(file)
-  SET(FILE_CONTENT ${FILE_CONTENT} "\n${CISST_STRING_POUND}endif // _${LIBRARY}_h\n")
-  FILE(WRITE ${LIBRARY_MAIN_HEADER_TMP} ${FILE_CONTENT})
-
-  EXEC_PROGRAM(${CMAKE_COMMAND}
-               ARGS -E copy_if_different
-               \"${LIBRARY_MAIN_HEADER_TMP}\"
-               \"${LIBRARY_MAIN_HEADER}\")
-
-  EXEC_PROGRAM(${CMAKE_COMMAND}
-               ARGS -E remove
-               \"${LIBRARY_MAIN_HEADER_TMP}\")
-
-  # Add the main header to the library, for IDEs
-  SET(HEADERS ${HEADERS} ${LIBRARY_MAIN_HEADER})
-
-  # Add the library
-  ADD_LIBRARY(${LIBRARY}
-              ${IS_SHARED}
-              ${SOURCES}
-              ${ADDITIONAL_SOURCE_FILES}
-              ${HEADERS}
-              ${ADDITIONAL_HEADER_FILES}
-              )
-  INSTALL_TARGETS(/lib ${LIBRARY})
-
-  # Add dependencies for linking, also check BUILD_xxx for dependencies
-  IF(DEPENDENCIES)
-    # Check that dependencies are build
-    FOREACH(dependency ${DEPENDENCIES})
-      SET(BUILD_DEPENDENCIES ${BUILD_DEPENDENCIES} CISST_BUILD_LIBS_${dependency})
-    ENDFOREACH(dependency)
-    VARIABLE_REQUIRES(CISST_BUILD_LIBS_${LIBRARY} CISST_BUILD_LIBS_${LIBRARY} ${BUILD_DEPENDENCIES})
-    # Set the link flags
-    TARGET_LINK_LIBRARIES(${LIBRARY} ${DEPENDENCIES})
-  ENDIF(DEPENDENCIES)
-
-  # Install all header files
-  INSTALL_FILES(/include/${LIBRARY}
-                ".h"
-                ${HEADERS})
-  INSTALL_FILES(/include/
-                ".h"
-                ${LIBRARY_MAIN_HEADER})
-
-ENDIF(CISST_BUILD_LIBS_${LIBRARY} OR BUILD_${LIBRARY})
-
+macro (CISST_ADD_LIBRARY_TO_PROJECT PROJECT_NAME)
+  message (SEND_ERROR "The macro CISST_ADD_LIBRARY_TO_PROJECT is now deprecated, use \"cisst_add_library\" instead.")
 ENDMACRO(CISST_ADD_LIBRARY_TO_PROJECT)
-
 
 
 # DEPRECATED, USE cisst_target_link_libraries INSTEAD
 # Macro used to compare required libraries for a given target with
 # libraries actually compiled.  This macro adds the required link
 # options.
-MACRO(CISST_REQUIRES WHO_REQUIRES REQUIRED_CISST_LIBRARIES)
-
-   IF(CISST_BUILD_SHARED_LIBS)
-     ADD_DEFINITIONS(-DCISST_DLL)
-   ENDIF(CISST_BUILD_SHARED_LIBS)
-
-   # First test that all libraries should have been compiled
-   FOREACH(required ${REQUIRED_CISST_LIBRARIES})
-     IF("${CISST_LIBRARIES}"  MATCHES ${required})
-     ELSE("${CISST_LIBRARIES}"  MATCHES ${required})
-       MESSAGE(SEND_ERROR "${WHO_REQUIRES} requires ${required} which doesn't exist or hasn't been compiled")
-     ENDIF("${CISST_LIBRARIES}"  MATCHES ${required})
-   ENDFOREACH(required)
-
-   # Second, create a list of libraries in the right order
-   FOREACH(existing ${CISST_LIBRARIES})
-     IF("${REQUIRED_CISST_LIBRARIES}" MATCHES ${existing})
-       SET(CISST_LIBRARIES_TO_USE ${CISST_LIBRARIES_TO_USE} ${existing})
-     ENDIF("${REQUIRED_CISST_LIBRARIES}" MATCHES ${existing})
-   ENDFOREACH(existing)
-
-   # Finally, link with the required libraries
-   TARGET_LINK_LIBRARIES(${WHO_REQUIRES} ${CISST_LIBRARIES_TO_USE})
-
-ENDMACRO(CISST_REQUIRES)
-
+macro (CISST_REQUIRES WHO_REQUIRES REQUIRED_CISST_LIBRARIES)
+  message (SEND_ERROR "The macro CISST_REQUIRES is now deprecated, use \"find_package (cisst REQUIRED cisstCommon ...)\" in combination with \"cisst_target_link_libraries\" instead")
+endmacro (CISST_REQUIRES)
 
 
 # function to add all the available tests
@@ -765,4 +670,3 @@ function (cisst_add_test ...)
   set_directory_properties (PROPERTIES TEST_INCLUDE_FILE
                             "${CMAKE_CURRENT_BINARY_DIR}/DartTestfile-${TEST_PROGRAM}.txt")
 endfunction (cisst_add_test)
-
