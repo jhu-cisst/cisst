0d6d48f48e53768200260ff4b5b5e38f9cb422e6
Peter Kazanzides <pkaz@jhu.edu>
2011-11-16 02:47:20 +0000

    Improved some error messages in cisstDevices include files.
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@3188 0959eb1a-6956-4f60-947b-6092a4e22c97

--- cisst/cisstDevices/robotcomponents/glut/devGLUT.h
+++ cisst/cisstDevices/robotcomponents/glut/devGLUT.h
@@ -1,6 +1,6 @@
 #ifndef _devGLUT_h
 #define _devGLUT_h
 
-#error "The class devGLUT is now deprecated - if you really need this class, please revert your SVN local copy to revision https://svn.lcsr.jhu.edu/cisst/tags/pre-saw/"
+#error "The class devGLUT has been replaced sawGLUTSimulator/osaGLUT; if you really need devGLUT, please revert your SVN local copy to revision https://svn.lcsr.jhu.edu/cisst/tags/pre-saw/"
 
 #endif
--- cisst/cisstDevices/robotcomponents/glut/devGLUTManipulator.h
+++ cisst/cisstDevices/robotcomponents/glut/devGLUTManipulator.h
@@ -1,6 +1,6 @@
 #ifndef _devGLUTManipulator_h
 #define _devGLUTManipulator_h
 
-#error "The class devGLUTManipulator is now deprecated - if you really need this class, please revert your SVN local copy to revision https://svn.lcsr.jhu.edu/cisst/tags/pre-saw/"
+#error "The class devGLUTManipulator has been replaced by sawGLUTSimulator/mtsGLUTManipulator; if you really need devGLUTManipulator please revert your SVN local copy to revision https://svn.lcsr.jhu.edu/cisst/tags/pre-saw/"
 
 #endif
