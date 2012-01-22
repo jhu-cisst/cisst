259a92a4428626670271015537ddbe4d9abf6ca9
Anton Deguet <anton.deguet@jhu.edu>
2012-01-22 02:52:34 +0000

    /trunk, vctQuaternionRotation3Base.cpp: fixed conversion from rotation matrix.  Fails less unit tests with higher number of iterations.  Tested two implementations, both have similar results with existing unit tests.
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@3367 0959eb1a-6956-4f60-947b-6092a4e22c97

--- cisst/cisstVector/code/vctQuaternionRotation3Base.cpp
+++ cisst/cisstVector/code/vctQuaternionRotation3Base.cpp
@@ -3,7 +3,7 @@
 
 /*
   $Id$
-  
+
   Author(s):	Anton Deguet
   Created on:	2005-08-24
 
@@ -52,38 +52,97 @@ vctQuaternionRotation3BaseFromRaw(vctQuaternionRotation3Base<_quaternionType> & 
     typedef typename _quaternionType::TypeTraits TypeTraits;
     typedef typename _quaternionType::NormType NormType;
 
+#if 0 // this seems to work fairly well
     NormType trace, traceInverse;
-    trace = NormType(1.0) + matrixRotation.Element(0, 0) + matrixRotation.Element(1, 1) + matrixRotation.Element(2, 2);
-    if (vctUnaryOperations<NormType>::AbsValue::Operate(trace) > cmnTypeTraits<NormType>::Tolerance()) {
-        trace = NormType(sqrt(trace) * 2.0);
+    trace = matrixRotation.Element(0, 0) + matrixRotation.Element(1, 1) + matrixRotation.Element(2, 2);
+
+    if (trace > cmnTypeTraits<NormType>::Tolerance()) {
+        trace = NormType(sqrt(trace + NormType(1.0)) * 2.0);
         traceInverse = NormType(1.0) / trace;
         quaternionRotation.X() = value_type((matrixRotation.Element(2, 1) - matrixRotation.Element(1, 2)) * traceInverse);
         quaternionRotation.Y() = value_type((matrixRotation.Element(0, 2) - matrixRotation.Element(2, 0)) * traceInverse);
         quaternionRotation.Z() = value_type((matrixRotation.Element(1, 0) - matrixRotation.Element(0, 1)) * traceInverse);
         quaternionRotation.R() = value_type(0.25 * trace);
-    } else 
+    } else  {
         if (matrixRotation.Element(0, 0) > matrixRotation.Element(1, 1) && matrixRotation.Element(0, 0) > matrixRotation.Element(2, 2))  {
             trace  = NormType(sqrt(1.0 + matrixRotation.Element(0, 0) - matrixRotation.Element(1, 1) - matrixRotation.Element(2, 2)) * 2.0);
             traceInverse = NormType(1.0) / trace;
             quaternionRotation.X() = value_type(0.25 * trace);
             quaternionRotation.Y() = value_type((matrixRotation.Element(0, 1) + matrixRotation.Element(1, 0)) * traceInverse);
             quaternionRotation.Z() = value_type((matrixRotation.Element(2, 0) + matrixRotation.Element(0, 2)) * traceInverse);
-            quaternionRotation.R() = value_type((matrixRotation.Element(1, 2) - matrixRotation.Element(2, 1)) * traceInverse);
+            quaternionRotation.R() = value_type((matrixRotation.Element(2, 1) - matrixRotation.Element(1, 2)) * traceInverse);
         } else if (matrixRotation.Element(1, 1) > matrixRotation.Element(2, 2)) {
             trace  = NormType(sqrt(1.0 + matrixRotation.Element(1, 1) - matrixRotation.Element(0, 0) - matrixRotation.Element(2, 2)) * 2.0);
             traceInverse = NormType(1.0) / trace;
             quaternionRotation.X() = value_type((matrixRotation.Element(0, 1) + matrixRotation.Element(1, 0)) * traceInverse);
             quaternionRotation.Y() = value_type(0.25 * trace);
             quaternionRotation.Z() = value_type((matrixRotation.Element(1, 2) + matrixRotation.Element(2, 1)) * traceInverse);
-            quaternionRotation.R() = value_type((matrixRotation.Element(2, 0) - matrixRotation.Element(0, 2)) * traceInverse);
+            quaternionRotation.R() = value_type((matrixRotation.Element(0, 2) - matrixRotation.Element(2, 0)) * traceInverse);
         } else {
             trace  = NormType(sqrt(1.0 + matrixRotation.Element(2, 2) - matrixRotation.Element(0, 0) - matrixRotation.Element(1, 1)) * 2.0);
             traceInverse = NormType(1.0) / trace;
             quaternionRotation.X() = value_type((matrixRotation.Element(2, 0) + matrixRotation.Element(0, 2)) * traceInverse);
             quaternionRotation.Y() = value_type((matrixRotation.Element(1, 2) + matrixRotation.Element(2, 1)) * traceInverse);
             quaternionRotation.Z() = value_type(0.25 * trace);
-            quaternionRotation.R() = value_type((matrixRotation.Element(0, 1) - matrixRotation.Element(1, 0)) * traceInverse);
+            quaternionRotation.R() = value_type((matrixRotation.Element(1, 0) - matrixRotation.Element(0, 1)) * traceInverse);
         }
+    }
+#endif
+
+#if 1 // should be the most reliable as we are looking for the highest "pivot"
+    const NormType a0 = 1.0 + matrixRotation.Element(0, 0) + matrixRotation.Element(1, 1) + matrixRotation.Element(2, 2);
+    const NormType a1 = 1.0 + matrixRotation.Element(0, 0) - matrixRotation.Element(1, 1) - matrixRotation.Element(2, 2);
+    const NormType a2 = 1.0 - matrixRotation.Element(0, 0) + matrixRotation.Element(1, 1) - matrixRotation.Element(2, 2);
+    const NormType a3 = 1.0 - matrixRotation.Element(0, 0) - matrixRotation.Element(1, 1) + matrixRotation.Element(2, 2);
+
+    NormType max = a0;
+    unsigned char index = 0;
+    if (a1 > max) {
+        max = a1;
+        index = 1;
+    }
+    if (a2 > max) {
+        max = a2;
+        index = 2;
+    }
+    if (a3 > max) {
+        index = 3;
+    }
+
+    NormType ratio;
+    switch (index) {
+    case 0:
+        quaternionRotation.R() = sqrt(a0) * 0.5;
+        ratio = 0.25 / quaternionRotation.R();
+        quaternionRotation.X() = (matrixRotation.Element(2, 1) - matrixRotation.Element(1, 2)) * ratio;
+        quaternionRotation.Y() = (matrixRotation.Element(0, 2) - matrixRotation.Element(2, 0)) * ratio;
+        quaternionRotation.Z() = (matrixRotation.Element(1, 0) - matrixRotation.Element(0, 1)) * ratio;
+        break;
+    case 1:
+        quaternionRotation.X() = sqrt(a1) * 0.5;
+        ratio = 0.25 / quaternionRotation.X();
+        quaternionRotation.R() = (matrixRotation.Element(2, 1) - matrixRotation.Element(1, 2)) * ratio;
+        quaternionRotation.Y() = (matrixRotation.Element(1, 0) + matrixRotation.Element(0, 1)) * ratio;
+        quaternionRotation.Z() = (matrixRotation.Element(2, 0) + matrixRotation.Element(0, 2)) * ratio;
+        break;
+    case 2:
+        quaternionRotation.Y() = sqrt(a2) * 0.5;
+        ratio = 0.25 / quaternionRotation.Y();
+        quaternionRotation.R() = (matrixRotation.Element(0, 2) - matrixRotation.Element(2, 0)) * ratio;
+        quaternionRotation.X() = (matrixRotation.Element(1, 0) + matrixRotation.Element(0, 1)) * ratio;
+        quaternionRotation.Z() = (matrixRotation.Element(2, 1) + matrixRotation.Element(1, 2)) * ratio;
+        break;
+    case 3:
+        quaternionRotation.Z() = sqrt(a3) * 0.5;
+        ratio = 0.25 / quaternionRotation.Z();
+        quaternionRotation.R() = (matrixRotation.Element(1, 0) - matrixRotation.Element(0, 1)) * ratio;
+        quaternionRotation.X() = (matrixRotation.Element(0, 2) + matrixRotation.Element(2, 0)) * ratio;
+        quaternionRotation.Y() = (matrixRotation.Element(2, 1) + matrixRotation.Element(1, 2)) * ratio;
+        break;
+    default:
+        break;
+    }
+#endif
 }
 
 
