5f1d1fb19ec414c61201812c7ca20a32f78bb8bb
Peter Kazanzides <pkaz@jhu.edu>
2012-04-23 13:20:30 +0000

    tests/vctPlot2D: renamed "trace" to "signal".
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@3609 0959eb1a-6956-4f60-947b-6092a4e22c97

--- cisst/cisstVector/tests/vctPlot2DBaseTest.cpp
+++ cisst/cisstVector/tests/vctPlot2DBaseTest.cpp
@@ -24,7 +24,7 @@ http://www.cisst.org/cisst/license.txt.
 void vctPlot2DBaseTest::TestBufferManipulating(void)
 {
     vctPlot2DBaseTestClass plot;
-    vctPlot2DBaseTestClass::Trace * trace = plot.AddTrace("TestTrace");
+    vctPlot2DBaseTestClass::Signal * signal = plot.AddSignal("TestSignal");
     const size_t dataElements = 1000;
     const size_t pointSize = 2;
     double * arrayToAppend = new double[dataElements * pointSize];
@@ -59,125 +59,125 @@ void vctPlot2DBaseTest::TestBufferManipulating(void)
     }
 
     /****** TEST AppendPoint() ******/
-    trace->SetSize(dataElements);
+    signal->SetSize(dataElements);
     // 0 ~ 99
     for (double i = 0; i < dataElements; i++) {
-        trace->AppendPoint(vctDouble2(i, i));
+        signal->AppendPoint(vctDouble2(i, i));
     }
 
     vctDouble2 point;
     for (i = 0; i < dataElements; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == i);
         CPPUNIT_ASSERT(point.Y() == i);
     }
 
     /****** TEST Resize(), Expand ******/
-    trace->Resize(dataElements * 2);
-    bufferSize = trace->GetSize();
-    numberOfPoints = trace->GetNumberOfPoints();
+    signal->Resize(dataElements * 2);
+    bufferSize = signal->GetSize();
+    numberOfPoints = signal->GetNumberOfPoints();
     CPPUNIT_ASSERT(numberOfPoints == dataElements);
     CPPUNIT_ASSERT(bufferSize == dataElements * 2);
     // 0 ~ 99, After expanding
     for (i = 0; i < dataElements; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == i);
         CPPUNIT_ASSERT(point.Y() == i);
     }
 
     /****** TEST AppendArray() ******/
-    CPPUNIT_ASSERT(trace->AppendArray(arrayToAppend, dataElements * pointSize));
+    CPPUNIT_ASSERT(signal->AppendArray(arrayToAppend, dataElements * pointSize));
 
     // 0~199, After append
     for (i = 0; i < dataElements * 2; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == i);
         CPPUNIT_ASSERT(point.Y() == i);
     }
 
     /****** TEST Resize(), Shrink, trimOlder = false  ******/
-    trace->Resize(dataElements, false);
-    bufferSize = trace->GetSize();
-    numberOfPoints = trace->GetNumberOfPoints();
+    signal->Resize(dataElements, false);
+    bufferSize = signal->GetSize();
+    numberOfPoints = signal->GetNumberOfPoints();
     CPPUNIT_ASSERT(numberOfPoints == dataElements);
     CPPUNIT_ASSERT(bufferSize == dataElements);
     //  0~99, After shrinking
     for (i = 0; i < dataElements; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == i);
         CPPUNIT_ASSERT(point.Y() == i);
     }
 
-    trace->AppendArray(defaultArray, dataElements * pointSize);
-    trace->Resize(dataElements * 2);
-    trace->PrependArray(arrayToPrepend, dataElements * pointSize);
+    signal->AppendArray(defaultArray, dataElements * pointSize);
+    signal->Resize(dataElements * 2);
+    signal->PrependArray(arrayToPrepend, dataElements * pointSize);
     // -100 ~ 99
     for (i = 0; i < dataElements * 2; i++){
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == (i - dataElements));
         CPPUNIT_ASSERT(point.Y() == (i - dataElements));
     }
 
     // Set array to 0 ~ 199
-    trace->Resize(dataElements * 2);
-    CPPUNIT_ASSERT(trace->AppendArray(arrayToAppend, dataElements * pointSize));
+    signal->Resize(dataElements * 2);
+    CPPUNIT_ASSERT(signal->AppendArray(arrayToAppend, dataElements * pointSize));
 
 
     /****** TEST Resize(), Shrink, trimOlder = true ******/
-    trace->Resize(dataElements);
-    bufferSize = trace->GetSize();
-    numberOfPoints = trace->GetNumberOfPoints();
+    signal->Resize(dataElements);
+    bufferSize = signal->GetSize();
+    numberOfPoints = signal->GetNumberOfPoints();
     CPPUNIT_ASSERT(numberOfPoints == dataElements);
     CPPUNIT_ASSERT(bufferSize == dataElements);
 
     // 100~199, After shrinking
     for (i = 0; i < dataElements; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == i + dataElements);
         CPPUNIT_ASSERT(point.Y() == i + dataElements);
     }
 
     /****** TEST Prepend() ******/
-    trace->Resize(dataElements * 2);
+    signal->Resize(dataElements * 2);
     // do twice, see if the data is replaced correctly
-    trace->PrependArray(arrayToAppend, dataElements * pointSize);
-    trace->PrependArray(arrayToPrepend, dataElements * pointSize);
-    bufferSize = trace->GetSize();
-    numberOfPoints = trace->GetNumberOfPoints();
+    signal->PrependArray(arrayToAppend, dataElements * pointSize);
+    signal->PrependArray(arrayToPrepend, dataElements * pointSize);
+    bufferSize = signal->GetSize();
+    numberOfPoints = signal->GetNumberOfPoints();
     CPPUNIT_ASSERT(numberOfPoints == dataElements * 2);
     CPPUNIT_ASSERT(bufferSize == dataElements * 2);
     for (i = dataElements; i < dataElements * 2 ; i++) {
-        trace->SetPointAt(i, vctDouble2(i - dataElements, i - dataElements));
+        signal->SetPointAt(i, vctDouble2(i - dataElements, i - dataElements));
     }
     // -100 ~ 99
     for (i = 0; i < dataElements * 2; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == (i - dataElements));
         CPPUNIT_ASSERT(point.Y() == (i - dataElements));
     }
     // test by overflowing  buffer
     // -99 ~ 100
-    trace->AppendPoint(vctDouble2(dataElements, dataElements));
-    trace->PrependArray(arrayToPrepend, dataElements * pointSize);
+    signal->AppendPoint(vctDouble2(dataElements, dataElements));
+    signal->PrependArray(arrayToPrepend, dataElements * pointSize);
     // -100~-1, -99 ~ 0
     for (i = 0; i < dataElements; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == (i - dataElements));
         CPPUNIT_ASSERT(point.Y() == (i - dataElements));
     }
     for (i = dataElements; i < dataElements * 2; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == (i % dataElements - dataElements + 1));
         CPPUNIT_ASSERT(point.Y() == (i % dataElements - dataElements + 1));
     }
 
     /****** TEST SetArrayAt() ******/
-    trace->SetArrayAt(0, arrayToPrepend, dataElements * pointSize);
-    trace->SetArrayAt(dataElements, arrayToPrepend, dataElements * pointSize);
+    signal->SetArrayAt(0, arrayToPrepend, dataElements * pointSize);
+    signal->SetArrayAt(dataElements, arrayToPrepend, dataElements * pointSize);
 
     // -100 ~ -1, -100~-1
     for (i = 0; i < dataElements * 2; i++) {
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == (i % dataElements - dataElements));
         CPPUNIT_ASSERT(point.Y() == (i % dataElements - dataElements));
     }
@@ -185,9 +185,9 @@ void vctPlot2DBaseTest::TestBufferManipulating(void)
     /****** TEST SetPointAt() ******/
     // -100 ~ -1, -100~-1
     for (i = 0; i < dataElements * 2; i++) {
-        point = trace->GetPointAt(i);
-        trace->SetPointAt(i, point);
-        point = trace->GetPointAt(i);
+        point = signal->GetPointAt(i);
+        signal->SetPointAt(i, point);
+        point = signal->GetPointAt(i);
         CPPUNIT_ASSERT(point.X() == (i % dataElements - dataElements));
         CPPUNIT_ASSERT(point.Y() == (i % dataElements - dataElements));
     }
@@ -199,7 +199,7 @@ void vctPlot2DBaseTest::TestBufferManipulating(void)
 void vctPlot2DBaseTest::TestRangeComputation(void){
 
     vctPlot2DBaseTestClass plot;
-    vctPlot2DBaseTestClass::Trace * trace = plot.AddTrace("TestRange");
+    vctPlot2DBaseTestClass::Signal * signal = plot.AddSignal("TestRange");
     const int dataElements = 12345;
     const int pointSize = 2;
     double * arrayToAppend = new double[dataElements * pointSize];
@@ -232,45 +232,45 @@ void vctPlot2DBaseTest::TestRangeComputation(void){
         j++;
     }
 
-    trace->SetSize(dataElements);
+    signal->SetSize(dataElements);
     for (double i = 1; i <= dataElements; i++) {
-        trace->AppendPoint(vctDouble2(i, i));
+        signal->AppendPoint(vctDouble2(i, i));
     }
 
     double  min, max;
     /****** TEST ComputeDataRangeX() , sorted = true******/
-    trace->ComputeDataRangeX(min, max, true);
+    signal->ComputeDataRangeX(min, max, true);
     CPPUNIT_ASSERT(min == 1.0) ;
     CPPUNIT_ASSERT(max == dataElements);
 
 	min = max = 0;
     /****** TEST ComputeDataRangeX() , sorted = false******/
-    trace->ComputeDataRangeX(min, max, false);
+    signal->ComputeDataRangeX(min, max, false);
     CPPUNIT_ASSERT(min == 1.0) ;
     CPPUNIT_ASSERT(max == dataElements);
 
 	min = max = 0;
     /****** TEST ComputeDataRangeY() ******/
-    trace->ComputeDataRangeY(min, max);
+    signal->ComputeDataRangeY(min, max);
     CPPUNIT_ASSERT(min == 1.0) ;
     CPPUNIT_ASSERT(max == dataElements);
 
 	min = max = 0;
     /****** TEST ComputeDataRangeX(), by overflowing buffer ******/
-    trace->AppendPoint(vctDouble2(0, 0));
-    trace->ComputeDataRangeX(min, max, false);
+    signal->AppendPoint(vctDouble2(0, 0));
+    signal->ComputeDataRangeX(min, max, false);
     CPPUNIT_ASSERT(min == 0.0) ;
     CPPUNIT_ASSERT(max == dataElements);
 
 	min = max = 0;
     /****** TEST ComputeDataRangeY(), by overflowing buffer ******/
-    trace->ComputeDataRangeY(min, max);
+    signal->ComputeDataRangeY(min, max);
     CPPUNIT_ASSERT(min == 0.0) ;
     CPPUNIT_ASSERT(max == dataElements);
     // I have to use static to declare
     vctDouble2 minXY, maxXY;
-    trace->AppendPoint(vctDouble2(dataElements + 1, dataElements + 1));
-    trace->ComputeDataRangeXY(minXY, maxXY);
+    signal->AppendPoint(vctDouble2(dataElements + 1, dataElements + 1));
+    signal->ComputeDataRangeXY(minXY, maxXY);
     CPPUNIT_ASSERT(minXY.X() == 0.0);
     CPPUNIT_ASSERT(minXY.Y() == 0.0);
     CPPUNIT_ASSERT(maxXY.X() == dataElements + 1);
