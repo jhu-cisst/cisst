493e83334f2584e2300807b66efd5389fbc0d803
Anton Deguet <anton.deguet@jhu.edu>
2012-05-14 16:44:03 +0000

    /branches/matlab: added script to wrap library call to mtlCreateComponent.
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/branches/2012-05-04-matlab@3633 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ cisst/cisstMatlab/code/matlab/mtlCreateComponent.m
@@ -0,0 +1,23 @@
+%
+% $Id$
+%
+% Author(s):  Anton Deguet
+% Created on: 2012-05-07
+%
+% (C) Copyright 2012 Johns Hopkins University (JHU), All Rights
+% Reserved.
+%
+% --- begin cisst license - do not edit ---
+%
+% This software is provided "as is" under an open source license, with
+% no warranty.  The complete license can be found in license.txt and
+% http://www.cisst.org/cisst/license.txt.
+%
+% --- end cisst license ---
+
+function mtlCreateComponent(componentName)
+    calllib('libcisstMatlab','mtlCreateComponent', componentName);
+    % push the newly created component to caller
+    command = ['assignin(''caller'', ''' componentName ''', ' componentName ');'];
+    eval(command);
+end
