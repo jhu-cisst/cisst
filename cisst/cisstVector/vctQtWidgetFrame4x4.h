b8092b5445806d70ef60453bad33e12822c8b25c
Anton Deguet <anton.deguet@jhu.edu>
2013-03-30 02:22:11 +0000

    /trunk, sawIntuitiveResearchKit: work in progress, added .rob files provided by S. Leonard, added shell for tele-operation, forward kinematic on master seems to work
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@4042 0959eb1a-6956-4f60-947b-6092a4e22c97

--- cisst/cisstVector/vctQtWidgetFrame4x4.h
+++ cisst/cisstVector/vctQtWidgetFrame4x4.h
@@ -26,6 +26,7 @@ http://www.cisst.org/cisst/license.txt.
 #include <QTableWidget>
 
 #include <cisstVector/vctFrame4x4.h>
+#include <cisstVector/vctFrameBase.h>
 
 // Always include last
 #include <cisstVector/vctExportQt.h>
@@ -37,6 +38,11 @@ class CISST_EXPORT vctQtWidgetFrame4x4DoubleRead: public QTableWidget
  public:
     vctQtWidgetFrame4x4DoubleRead(void);
     virtual bool SetValue(const vctFrame4x4<double> & value);
+
+    template <class _rotationType>
+    bool SetValue(const vctFrameBase<_rotationType> & value) {
+        SetValue(vctFrame4x4<double>(value));
+    }
 };
 
 class CISST_EXPORT vctQtWidgetFrame4x4DoubleWrite: public QTableWidget
