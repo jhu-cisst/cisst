13fcb3d97d1616777078a996c24238a684fff1c1
Anton Deguet <anton.deguet@jhu.edu>
2013-07-05 23:26:51 +0000

    /trunk, cisstRobot: function, function Rn and quintic: added tab configuration and applied 4 spaces tabs, added Set methods for dynamic vectors to modify trajectory after creation
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@4316 0959eb1a-6956-4f60-947b-6092a4e22c97

--- cisst/cisstRobot/robFunctionRn.h
+++ cisst/cisstRobot/robFunctionRn.h
@@ -1,9 +1,11 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
 /*
-
   Author(s): Simon Leonard
-  Created on: Nov 11 2009
+  Created on: 2009-11-11
 
-  (C) Copyright 2008 Johns Hopkins University (JHU), All Rights
+  (C) Copyright 2009-2013 Johns Hopkins University (JHU), All Rights
   Reserved.
 
 --- begin cisst license - do not edit ---
@@ -25,65 +27,76 @@ http://www.cisst.org/cisst/license.txt.
 #include <cisstRobot/robExport.h>
 
 class CISST_EXPORT robFunctionRn : public robFunction {
-  
+
  protected:
 
-  vctDynamicVector<double>   y1;  // initial value
-  vctDynamicVector<double>  y1d;  // initial 1st derivative
-  vctDynamicVector<double> y1dd;  // initial 2nd derivative
+    vctDynamicVector<double>   y1;  // initial value
+    vctDynamicVector<double>  y1d;  // initial 1st derivative
+    vctDynamicVector<double> y1dd;  // initial 2nd derivative
 
-  vctDynamicVector<double>   y2;  // final value
-  vctDynamicVector<double>  y2d;  // final 1st derivative
-  vctDynamicVector<double> y2dd;  // final 2nd derivative
-  
+    vctDynamicVector<double>   y2;  // final value
+    vctDynamicVector<double>  y2d;  // final 1st derivative
+    vctDynamicVector<double> y2dd;  // final 2nd derivative
+
  public:
 
-  robFunctionRn( double t1, 
-		 const vctFixedSizeVector<double,3>& y1, 
-		 const vctFixedSizeVector<double,3>& y1d, 
-		 const vctFixedSizeVector<double,3>& y1dd, 
-		 double t2, 
-		 const vctFixedSizeVector<double,3>& y2,
-		 const vctFixedSizeVector<double,3>& y2d,
-		 const vctFixedSizeVector<double,3>& y2dd );
+    robFunctionRn( void );
 
-  robFunctionRn( double t1, 
-		 const vctDynamicVector<double>& y1, 
-		 const vctDynamicVector<double>& y1d, 
-		 const vctDynamicVector<double>& y1dd, 
-		 double t2, 
-		 const vctDynamicVector<double>& y2,
-		 const vctDynamicVector<double>& y2d,
-		 const vctDynamicVector<double>& y2dd );
+    robFunctionRn( double t1,
+                   const vctFixedSizeVector<double,3>& p1,
+                   const vctFixedSizeVector<double,3>& p1d,
+                   const vctFixedSizeVector<double,3>& p1dd,
+                   double t2,
+                   const vctFixedSizeVector<double,3>& p2,
+                   const vctFixedSizeVector<double,3>& p2d,
+                   const vctFixedSizeVector<double,3>& p2dd );
 
-  void InitialState( vctDynamicVector<double>& y,
-		     vctDynamicVector<double>& yd,
-		     vctDynamicVector<double>& ydd );
+    robFunctionRn( double t1,
+                   const vctDynamicVector<double>& p1,
+                   const vctDynamicVector<double>& p1d,
+                   const vctDynamicVector<double>& p1dd,
+                   double t2,
+                   const vctDynamicVector<double>& p2,
+                   const vctDynamicVector<double>& p2d,
+                   const vctDynamicVector<double>& p2dd );
 
-  void FinalState( vctDynamicVector<double>& y,
-		   vctDynamicVector<double>& yd,
-		   vctDynamicVector<double>& ydd );
+    void Set( double t1,
+              const vctDynamicVector<double>& p1,
+              const vctDynamicVector<double>& p1d,
+              const vctDynamicVector<double>& p1dd,
+              double t2,
+              const vctDynamicVector<double>& p2,
+              const vctDynamicVector<double>& p2d,
+              const vctDynamicVector<double>& p2dd );
 
-  void InitialState( vctFixedSizeVector<double,3>& p, double& v, double& vd );
-  void FinalState  ( vctFixedSizeVector<double,3>& p, double& v, double& vd );
+    void InitialState( vctDynamicVector<double>& p,
+                       vctDynamicVector<double>& pd,
+                       vctDynamicVector<double>& pdd );
 
-  virtual void Evaluate( double, 
-			 vctFixedSizeVector<double,3>&, 
-			 vctFixedSizeVector<double,3>&, 
-			 vctFixedSizeVector<double,3>& ){}
+    void FinalState( vctDynamicVector<double>& p,
+                     vctDynamicVector<double>& pd,
+                     vctDynamicVector<double>& pdd );
 
-  virtual void Evaluate( double, 
-			 vctDynamicVector<double>&, 
-			 vctDynamicVector<double>&, 
-			 vctDynamicVector<double>& ) {}
-  
-  virtual void Blend( robFunction* function, 
-		      const vctDynamicVector<double>& qd, 
-		      const vctDynamicVector<double>& qdd ) = 0;
+    void InitialState( vctFixedSizeVector<double,3>& p, double& v, double& vd );
+    void FinalState  ( vctFixedSizeVector<double,3>& p, double& v, double& vd );
 
-  virtual void Blend( robFunction* function, double vmax, double vdmax ) = 0;
+    virtual void Evaluate( double,
+                           vctFixedSizeVector<double,3>&,
+                           vctFixedSizeVector<double,3>&,
+                           vctFixedSizeVector<double,3>& ){}
+
+    virtual void Evaluate( double,
+                           vctDynamicVector<double>&,
+                           vctDynamicVector<double>&,
+                           vctDynamicVector<double>& ) {}
+
+    virtual void Blend( robFunction* function,
+                        const vctDynamicVector<double>& qd,
+                        const vctDynamicVector<double>& qdd ) = 0;
+
+    virtual void Blend( robFunction* function, double vmax, double vdmax ) = 0;
 
 
 };
 
-#endif
+#endif // _robFunctionRn_h
