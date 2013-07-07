a86de01ba4fb12b36d056efcc039adcdd75d85d2
Anton Deguet <anton.deguet@jhu.edu>
2013-07-07 23:34:17 +0000

    /trunk, vctPlot2DOpenGL: removed unused variable
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@4324 0959eb1a-6956-4f60-947b-6092a4e22c97

--- cisst/cisstVector/vctDynamicVectorBase.h
+++ cisst/cisstVector/vctDynamicVectorBase.h
@@ -237,7 +237,7 @@ public:
 
 
     /*!
-      \name Assignment operation between vectors containing the same data type but different owners
+      Assignment operation between vectors containing the same data type but different owners
       \param other The vector to be copied.
     */
     //@{
@@ -255,6 +255,22 @@ public:
     }
     //@}
 
+    /*!
+      Assignment operation between vectors containing the same data type but different owners and sizes.
+      The number of elements to be copied must be lower or equal to the vector's size.
+
+      \param other The vector to be copied.
+    */
+    //@{
+    template <class __vectorOwnerType>
+    inline ThisType & Assign(const vctDynamicConstVectorBase<__vectorOwnerType, value_type> & other,
+                             size_type length, size_type startPositionThis = 0, size_type startPositionOther = 0) {
+        vctDynamicVectorRef<value_type> thisRef(*this, startPositionThis, length);
+        vctDynamicConstVectorRef<value_type> otherRef(other, startPositionOther, length);
+        thisRef.Assign(otherRef);
+        return *this;
+    }
+    //@}
 
     /*!
       \name Assignment operation between vectors of different types.
