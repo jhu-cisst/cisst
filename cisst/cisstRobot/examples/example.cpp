8c9691b797c0de7bd39eee664c22c902e8c80b23
Zihan Chen <zihan.chen@jhu.edu>
2013-08-15 02:04:05 +0000

    /trunk: cisstRobot - added examples folder and a dummy example code
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@4401 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ cisst/cisstRobot/examples/example.cpp
@@ -0,0 +1,26 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+/*
+  $Id: example.cpp 4278 2013-06-19 18:57:40Z adeguet1 $
+
+  Author(s):  Zihan Chen
+  Created on: 2013-08-14
+
+  (C) Copyright 2013 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+
+*/
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "hello cisstRobot" << std::endl;
+}
