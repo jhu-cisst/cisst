e894748a8f66b7d5ae87e145c971d6451300ef5a
Anton Deguet <anton.deguet@jhu.edu>
2013-12-22 23:15:11 +0000

    /trunk, cisstCommon: added cmnKbHit
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk@4638 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ cisst/cisstCommon/cmnKbHit.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  $Id$
+
+  Author(s):  Anton Deguet, Balazs Vagvolgyi
+  Created on: 2009-03-26
+
+  (C) Copyright 2009 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+
+*/
+
+#pragma once
+
+#ifndef _cmnKbHit_h
+#define _cmnKbHit_h
+
+#include <cisstCommon/cmnPortability.h>
+
+// Always the last cisst include
+#include <cisstCommon/cmnExport.h>
+
+int CISST_EXPORT cmnKbHit(void);
+
+#endif // _cmnKbHit_h
+
