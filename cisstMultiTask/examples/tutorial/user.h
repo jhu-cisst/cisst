eaca7e580c805d03295274e3af64b7e4dcd4972d
Anton Deguet <anton.deguet@jhu.edu>
2014-05-20 09:50:53 -0400

    fixed CISST_EXPORT for mts tutorial

--- cisstMultiTask/examples/tutorial/user.h
+++ cisstMultiTask/examples/tutorial/user.h
@@ -22,16 +22,7 @@ http://www.cisst.org/cisst/license.txt.
 
 #include <cisstMultiTask/mtsTaskContinuous.h>
 
-// check if this module is built as a DLL
-#ifdef mtsExTutorial_EXPORTS
-#define CISST_THIS_LIBRARY_AS_DLL
-#endif
-#include <cisstCommon/cmnExportMacros.h>
-
-// avoid impact on other modules
-#undef CISST_THIS_LIBRARY_AS_DLL
-
-class CISST_EXPORT user: public mtsTaskContinuous {
+class user: public mtsTaskContinuous {
     // used to control the log level, "Run Error" by default
     CMN_DECLARE_SERVICES(CMN_NO_DYNAMIC_CREATION, CMN_LOG_LOD_RUN_ERROR);
 
