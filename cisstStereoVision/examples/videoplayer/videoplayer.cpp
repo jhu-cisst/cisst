680a2c6920ffa71ec80aa4ec752ae53e2fe495af
vagvolgyi <balazs@thesphere.com>
2014-10-13 10:44:33 -0400

    cisstStereoVision: VideoPlayer example: image resizer option added

--- cisstStereoVision/examples/videoplayer/videoplayer.cpp
+++ cisstStereoVision/examples/videoplayer/videoplayer.cpp
@@ -28,6 +28,7 @@ http://www.cisst.org/cisst/license.txt.
 
 #include <cisstStereoVision/svlWindowManagerBase.h>
 #include <cisstStereoVision/svlFilterSourceVideoFile.h>
+#include <cisstStereoVision/svlFilterImageResizer.h>
 #include <cisstStereoVision/svlFilterImageWindow.h>
 #include <cisstStereoVision/svlFilterImageOverlay.h>
 
@@ -126,6 +127,7 @@ int VideoPlayer(std::string pathname)
     // instantiating SVL stream and filters
     svlStreamManager stream(4);
     svlFilterSourceVideoFile source(1);
+    svlFilterImageResizer resizer;
     svlFilterImageOverlay overlay;
 #if _USE_QT_ && CISST_HAS_OPENGL
     svlFilterImageWindowQt window;
@@ -133,6 +135,9 @@ int VideoPlayer(std::string pathname)
     svlFilterImageWindow window;
 #endif // _USE_QT_ && CISST_HAS_OPENGL
     CViewerEventHandler window_cb;
+
+    // setup resizer
+    resizer.SetOutputRatio(0.5, 0.5);
 
     // setup overlay
     svlOverlayFramerate ovrl_fps(0, true, &overlay, svlRect(4, 24, 49, 41),
@@ -172,7 +177,12 @@ int VideoPlayer(std::string pathname)
 
     // chain filters to pipeline
     stream.SetSourceFilter(&source);
+#if 0
+    source.GetOutput()->Connect(resizer.GetInput());
+    resizer.GetOutput()->Connect(overlay.GetInput());
+#else
     source.GetOutput()->Connect(overlay.GetInput());
+#endif
     overlay.GetOutput()->Connect(window.GetInput());
 
     cerr << endl << "Starting stream... " << endl;
