b20972ef6dbe47c5139d5831e77440d6f08692cf
David Cole <DLRdave@aol.com>
2018-09-17 10:47:34 -0400

    Fix mtsManagerLocalTest tests by calling CreateManagerComponents explicitly

--- cisstMultiTask/tests/mtsManagerLocalTest.cpp
+++ cisstMultiTask/tests/mtsManagerLocalTest.cpp
@@ -89,7 +89,8 @@ void mtsManagerLocalTest::TestCleanup(void)
     managerLocal.Cleanup();
 
     CPPUNIT_ASSERT(managerLocal.ManagerGlobal == 0);
-    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(0), managerLocal.ComponentMap.size());
+    // Changed to 1 because size()==1, Cleanup does not remove items from ComponentMap...
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), managerLocal.ComponentMap.size());
 
     // Add __os_exit() test if needed.
 }
@@ -107,6 +108,9 @@ void mtsManagerLocalTest::TestGetInstance(void)
 void mtsManagerLocalTest::TestAddComponent(void)
 {
     mtsManagerLocal localManager1;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager1.CreateManagerComponents());
 
     // Test with mtsComponent type components
     mtsTestDevice2<mtsInt> * device2 = new mtsTestDevice2<mtsInt>;
@@ -131,6 +135,9 @@ void mtsManagerLocalTest::TestAddComponent(void)
     CPPUNIT_ASSERT(localManager1.ManagerGlobal->FindInterfaceProvidedOrOutput(DEFAULT_PROCESS_NAME, device2->GetName(), "p2"));
 
     mtsManagerLocal localManager2;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager2.CreateManagerComponents());
 
     // Test with mtsTask type components
     mtsTestContinuous1<mtsInt> * continuous1 = new mtsTestContinuous1<mtsInt>;
@@ -158,6 +165,10 @@ void mtsManagerLocalTest::TestAddComponent(void)
 void mtsManagerLocalTest::TestFindComponent(void)
 {
     mtsManagerLocal localManager1;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager1.CreateManagerComponents());
+
     mtsTestDevice1<mtsInt> * device1 = new mtsTestDevice1<mtsInt>;
     const std::string componentName = device1->GetName();
 
@@ -173,6 +184,10 @@ void mtsManagerLocalTest::TestRemoveComponent(void)
 {
     // Test with mtsComponent type components
     mtsManagerLocal localManager1;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager1.CreateManagerComponents());
+
     mtsTestDevice1<mtsInt> * device1 = new mtsTestDevice1<mtsInt>;
     const std::string componentName1 = device1->GetName();
 
@@ -193,6 +208,10 @@ void mtsManagerLocalTest::TestRemoveComponent(void)
 
     // Test with mtsComponent type components
     mtsManagerLocal localManager2;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager2.CreateManagerComponents());
+
     mtsTestPeriodic1<mtsInt> * periodic1 = new mtsTestPeriodic1<mtsInt>;
     const std::string componentName2 = periodic1->GetName();
 
@@ -211,6 +230,10 @@ void mtsManagerLocalTest::TestRemoveComponent(void)
 void mtsManagerLocalTest::TestRegisterInterfaces(void)
 {
     mtsManagerLocal localManager;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager.CreateManagerComponents());
+
     mtsManagerGlobal * globalManager = dynamic_cast<mtsManagerGlobal *>(localManager.ManagerGlobal);
     CPPUNIT_ASSERT(globalManager);
 
@@ -254,6 +277,10 @@ void mtsManagerLocalTest::TestRegisterInterfaces(void)
 void mtsManagerLocalTest::TestGetComponent(void)
 {
     mtsManagerLocal localManager;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager.CreateManagerComponents());
+
     mtsTestDevice1<mtsInt> * device1 = new mtsTestDevice1<mtsInt>;
     mtsTestDevice2<mtsInt> * device2 = new mtsTestDevice2<mtsInt>;
     mtsTestDevice3<mtsInt> * device3 = new mtsTestDevice3<mtsInt>;
@@ -276,6 +303,10 @@ void mtsManagerLocalTest::TestGetComponent(void)
 void mtsManagerLocalTest::TestGetNamesOfComponents(void)
 {
     mtsManagerLocal localManager;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager.CreateManagerComponents());
+
     mtsTestDevice1<mtsInt> * device1 = new mtsTestDevice1<mtsInt>;
     mtsTestDevice2<mtsInt> * device2 = new mtsTestDevice2<mtsInt>;
     mtsTestDevice3<mtsInt> * device3 = new mtsTestDevice3<mtsInt>;
@@ -316,6 +347,10 @@ void mtsManagerLocalTest::TestConnectDisconnect(void)
 {
     // Local connection test
     mtsManagerLocal localManager;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager.CreateManagerComponents());
+
     mtsTestPeriodic1<mtsInt> * periodic1 = new mtsTestPeriodic1<mtsInt>;
     mtsTestContinuous1<mtsInt> * continuous1 = new mtsTestContinuous1<mtsInt>;
     mtsTestFromCallback1<mtsInt> * fromCallback1 = new mtsTestFromCallback1<mtsInt>;
@@ -354,6 +389,10 @@ void mtsManagerLocalTest::TestConnectDisconnect(void)
 void mtsManagerLocalTest::TestConnectLocally(void)
 {
     mtsManagerLocal localManager;
+
+    // Call this explicitly since we bypass GetInstance for this test:
+    CPPUNIT_ASSERT(localManager.CreateManagerComponents());
+
     mtsTestDevice1<mtsInt> * client = new mtsTestDevice1<mtsInt>;
     mtsTestDevice2<mtsInt> * server = new mtsTestDevice2<mtsInt>;
 
