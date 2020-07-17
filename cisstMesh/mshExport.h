c116b8865c95255834f21fa147f7f8422dd7eec7
Anton Deguet <anton.deguet@jhu.edu>
2020-07-17 12:11:03 -0400

    cisstMesh: added empty files to create libs/tests/examples/python

---
+++ cisstMesh/mshExport.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  Author(s): Anton Deguet
+  Created on: 2020-07-17
+
+  (C) Copyright 2020 Johns Hopkins University (JHU), All Rights Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+/*! \file
+    \brief Macros to export the symbols of cisstMesh (in a Dll).
+*/
+
+// check if this module is build as a DLL
+#ifdef cisstMesh_EXPORTS
+#define CISST_THIS_LIBRARY_AS_DLL
+#endif
+
+// include common defines
+#include <cisstCommon/cmnExportMacros.h>
+
+// avoid impact on other modules
+#undef CISST_THIS_LIBRARY_AS_DLL
