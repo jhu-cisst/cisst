3ee4bbabc5aef9592c55506c5794fc661cba88fa
Anton Deguet <anton.deguet@jhu.edu>
2020-08-21 09:35:53 -0400

    travis CI: working on using cisstNetlib

--- .travis.cache.cmake
+++ .travis.cache.cmake
@@ -9,3 +9,6 @@ set (CISST_HAS_JSON ON CACHE BOOL "")
 # Unit tests
 set (CISST_BUILD_TESTS ON CACHE BOOL "")
 
+# cisstNetlib
+set (CISST_HAS_CISSTNETLIB ON CACHE BOOL "")
+set (CISSTNETLIB_DIR "/home/anton/catkin_ws/devel" CACHE PATH "")
