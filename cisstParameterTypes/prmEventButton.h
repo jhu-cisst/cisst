a9e394489c64f2d523a18c15e428462df894f871
Anton Deguet <anton.deguet@jhu.edu>
2020-09-17 15:36:12 -0400

    prmEventButton: added enum value ERROR

--- cisstParameterTypes/prmEventButton.h
+++ cisstParameterTypes/prmEventButton.h
@@ -38,7 +38,7 @@ class CISST_EXPORT prmEventButton: public mtsGenericObject
 
  public:
     typedef mtsGenericObject BaseType;
-    typedef enum {PRESSED, RELEASED, CLICKED, DOUBLE_CLICKED} EventType;
+    typedef enum {PRESSED, RELEASED, CLICKED, DOUBLE_CLICKED, ERROR} EventType;
 
  public:
     /*! Default constructor */
