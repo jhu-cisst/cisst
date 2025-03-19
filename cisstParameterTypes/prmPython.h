6c23430347ed26eef92da3e46a7b63a18efe1149
Peter Kazanzides <pkaz@jhu.edu>
2025-03-19 00:55:17 -0400

    Swig wrapping of prmKeyValue (for Python)

--- cisstParameterTypes/prmPython.h
+++ cisstParameterTypes/prmPython.h
@@ -5,7 +5,7 @@
   Author(s):	Anton Deguet
   Created on:	2009-11-08
 
-  (C) Copyright 2009-2024 Johns Hopkins University (JHU), All Rights Reserved.
+  (C) Copyright 2009-2025 Johns Hopkins University (JHU), All Rights Reserved.
 
 --- begin cisst license - do not edit ---
 
@@ -54,5 +54,6 @@ http://www.cisst.org/cisst/license.txt.
 #include <cisstParameterTypes/prmImageFrame.h>
 #include <cisstParameterTypes/prmCameraInfo.h>
 #include <cisstParameterTypes/prmDepthMap.h>
+#include <cisstParameterTypes/prmKeyValue.h>
 
 #endif // _prmPython_h
